766887066bcc1eb5cdb4da868b8052e04bd097fd
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-05-23 14:11:53 +0000

    Make the selection between name and type search better, previously a->a was considered a name

--- src/Hoogle/Search.hs
+++ src/Hoogle/Search.hs
@@ -9,19 +9,27 @@ import Char
 
 
 data Search = Search String SearchMode
+              deriving Show
 
 
 data SearchMode = SearchName String
                 | SearchType ConType
                 | SearchError String
+                deriving Show
 
 
 parseSearch :: String -> Search
 parseSearch x = Search x $
-                if all (not . isSpace) x2
+                if isHaskellName x2
                 then SearchName x2
                 else case parseConType x2 of
                          Right x -> SearchError x
                          Left x -> SearchType x
     where
         x2 = trim x
+
+
+isHaskellName :: String -> Bool
+isHaskellName (x:xs) | isAlpha x && all (\a -> isAlphaNum a || a `elem` "_'") xs = True
+isHaskellName xs = all (`elem` "!#$%&*+./<>=?@/^|-~") xs
+isHaskellName _ = False
