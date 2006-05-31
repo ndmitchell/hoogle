de3af974c20381e45b797c2688b65d9f80adffab
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-05-31 16:13:28 +0000

    Change the copyright year on the command line version

--- src/CmdLine/Main.hs
+++ src/CmdLine/Main.hs
@@ -108,7 +108,7 @@ helpMsg :: String
 helpMsg
     = unlines $ [
         "HOOGLE - Haskell API Search",
-        "(C) Neil Mitchell 2004-2005, York University, UK",
+        "(C) Neil Mitchell 2004-2006, York University, UK",
         "",
         usageInfo ("Usage: hoogle [OPTION...] search") opts,
         
