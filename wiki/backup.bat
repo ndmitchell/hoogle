33f22f23a89bdf8147d0bbcc477b2bc3e8c88a72
Neil Mitchell <unknown>
2006-08-17 19:33:30 +0000

    Remove the wiki backup, the new wiki doesn't require it

--- wiki/backup.bat
+++
@@ -1,7 +0,0 @@
-REM First get the library docs
-wget http://www.haskell.org/hawiki/LibraryDocumentation --html-extension --recursive --level=1 --include-directories=hawiki
-copy www.haskell.org\hawiki\LibraryDocumentation*.* .
-
-REM And now specific hoogle related pages
-wget http://www.haskell.org/hawiki/Hoogle --html-extension
-wget http://www.haskell.org/hawiki/Keywords --html-extension
