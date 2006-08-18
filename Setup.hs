e2d3275921d5708a6728b976f0f4414c7508188f
Neil Mitchell <unknown>
2006-08-18 15:33:37 +0000

    Fix up the cabal stuff, preprocessing now works, thanks to Esa for lots of help

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,21 @@
 #!/usr/bin/env runhaskell
 import Distribution.Simple
-main = defaultMain
+import Distribution.Program
+import Distribution.PreProcess
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+import System.Exit
+
+main = defaultMainWithHooks userHooks
+    where
+        userHooks = defaultUserHooks{
+                            hookedPrograms = simpleProgram "trhsx" : hookedPrograms defaultUserHooks,
+                            hookedPreProcessors = ("hsx",trhxs) : hookedPreProcessors defaultUserHooks
+                        }
+        
+        trhxs :: BuildInfo -> LocalBuildInfo -> FilePath -> FilePath -> Int -> IO ExitCode
+        trhxs build local inFile outFile verbose = do
+                trhxs_exe <- lookupProgram "trhsx" (withPrograms local)
+                case trhxs_exe of
+                    Nothing -> error $ "trhsx not found, cannot process: " ++ inFile
+                    Just prog -> rawSystemProgram verbose prog [inFile, outFile]
