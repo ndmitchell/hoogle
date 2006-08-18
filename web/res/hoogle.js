a1b91f731328b679259260a44edbf8a575867ecb
Neil Mitchell <unknown>
2006-08-18 16:00:34 +0000

    Move the javascript out of the HTML and into a separate file

---
+++ web/res/hoogle.js
@@ -0,0 +1,24 @@
+// registered with the body onload event
+function on_load()
+{
+	document.getElementById('txt').focus();
+}
+
+// add hoogle as a quicksearch tag, available on Firefox/Mozilla
+function addHoogle()
+{
+	addEngine('hoogle','png','Programming','4691');
+}
+
+function addEngine(name,ext,cat,pid)
+{
+	if ((typeof window.sidebar == "object") && (typeof window.sidebar.addSearchEngine == "function"))
+	{
+		window.sidebar.addSearchEngine(
+			'http://mycroft.mozdev.org/install.php/' + pid + "/" + name + ".src",
+			'http://mycroft.mozdev.org/install.php/' + pid + "/" + name + "."+ ext, name, cat);
+	} else {
+		alert("You will need a browser which supports Sherlock to install this plugin.");
+	}
+}
+
