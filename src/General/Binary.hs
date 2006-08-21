9e27008b5886028fc5c0875d8a61a499dd6e6d61
Neil Mitchell <unknown>
2006-08-21 16:41:05 +0000

    Use the new hPutInt from the mailing list

--- src/General/Binary.hs
+++ src/General/Binary.hs
@@ -3,21 +3,26 @@ module General.Binary where
 
 import System.IO
 import Control.Monad
+import Data.Bits
+import Data.Char
 
-
--- > hPutInt h = hPutStr h . map chr . map (0xff .&.)
--- >                       . take 4 . iterate (`shiftR` 8)
--- >
--- > hGetInt h = replicateM 4 (hGetChar h) >>=
--- >             return . foldr (\i d -> i `shiftL` 8 .|. ord d) 0
 
 hPutInt :: Handle -> Int -> IO ()
-hPutInt hndl n = hPutStr hndl ('#' : replicate (8-length s) '0' ++ s)
-    where s = show n
-
+hPutInt h = hPutStr h . map chr . map (0xff .&.)
+                      . take 4 . iterate (`shiftR` 8)
 
 hGetInt :: Handle -> IO Int
-hGetInt hndl = do
+hGetInt h = replicateM 4 (hGetChar h) >>=
+            return . foldr (\d i -> i `shiftL` 8 .|. ord d) 0
+
+
+
+hPutIntText :: Handle -> Int -> IO ()
+hPutIntText hndl n = hPutStr hndl ('#' : replicate (8-length s) '0' ++ s)
+    where s = show n
+
+hGetIntText :: Handle -> IO Int
+hGetIntText hndl = do
     ('#':str) <- replicateM 9 $ hGetChar hndl
     return $ read str
 
@@ -27,7 +32,6 @@ hPutString :: Handle -> String -> IO ()
 hPutString hndl str = do
     hPutInt hndl (length str)
     hPutStr hndl str
-
 
 hGetString :: Handle -> IO String
 hGetString hndl = do
