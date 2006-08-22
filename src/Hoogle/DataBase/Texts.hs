d790e4e96191cca7d28c4acab5166f65c833a680
Neil Mitchell <unknown>
2006-08-22 21:05:26 +0000

    Use the new Tree format

--- src/Hoogle/DataBase/Texts.hs
+++ src/Hoogle/DataBase/Texts.hs
@@ -14,104 +14,115 @@ DESIGN 2
 
 Have a list of the sorted names
 
-[map],_id_,0
 [ap],_id_,1
+[map],_id_,0
 [p],_id_,2
 
-Then the tree points at a start and an end, with a number
-saying how many are exact
+_id_ is the id (int) of each element
+the names are sorted
+
+each is 8 bytes long
+
+The tree then is defined as:
+
+    List (count::Int) (items::[(Char,Int)]) (results::(Int,Int,Int))
+|
+    Trie (items::[Int*26]) (results::(Int,Int,Int))
+    
+    for Trie 0 = go nowhere, no results
+    
+    
+Results are given as
+    start position in the list
+    count of exact items
+    count of all
+
+    0,0,0 == no results
 
 -}
 
 
 -- True = Trie, False = List
-data TreeMode a = TreeNone
-                | TreeTail String a
-                | TreeList [(Char, Tree a)]
-                | TreeTrie [(Char, Tree a)]
-
+data TreeMode = TreeTrie | TreeList
 
 -- the Tree that is created during saveTexts
 -- a is the result thingy
-data Tree a = Tree Int (TreeMode a) [a]
+data Tree = Tree Int TreeMode [(Char,Tree)] (Int,Int,Int)
 
+treeSize (Tree a _ _ _) = a
 
 -- input list must be sorted already!
-buildTree :: [(String, a)] -> Tree a
-buildTree xs = Tree 0 (makeMode $ map f ys) (map snd blank)
+buildTree :: Int -> [String] -> Tree
+buildTree n xs = Tree 0 (makeMode res) res (n,length blank,length xs)
     where
-        (blank, non) = span (null . fst) xs
+        (blank, non) = span null xs
         
-        ys = groupBy cmp xs
-            where cmp (a:_,_) (b:_,_) = a == b
+        res = f (n + length blank) ys
         
-        f zs@((c,_):_) = (head c, buildTree [(tail a,b) | (a,b) <- zs])
+        ys = groupBy cmp non
+            where cmp a b = head a == head b
+
+        f n [] = []
+        f n (x:xs) = (head $ head x, buildTree n $ map tail x) : f (n + length x) xs
         
-        makeMode [] = TreeNone
-        makeMode x | length xs == 1 = let [(a,b)] = xs in TreeTail a b
-                   | all (isLetter . fst) x && length x > 10 = TreeTrie x
-                   | otherwise = TreeList x
+        makeMode xs = if all (isLetter . fst) xs && length xs > 10
+                      then TreeTrie else TreeList
 
         isLetter x = x >= 'a' && x <= 'z'
 
 
 -- id of the item, position in the item
-writeTree :: Handle -> Tree (Int, Int) -> IO ()
-writeTree hndl (Tree n xs ys) = return ()
+writeTree :: Handle -> Tree -> IO ()
+writeTree hndl (Tree n mode xs (r1,r2,r3)) = do
+        f mode
+        mapM_ (hPutInt hndl) [r1,r2,r2]
     where
-        f TreeNone = hPutByte hndl 0
+        mysize = n - sum (map (treeSize . snd) xs)
+        sxs = getSize n xs
         
-        f (TreeTail a b) = do
-            hPutByte hndl 1
-            hPutStr hndl a
-            
-        f (TreeList xs) = do
-            i <- hTellInt hndl
-            let nxs = length xs
-            hPutByte hndl 2
-            hPutInt hndl nxs
-            g (i+sizeByte+sizeInt + ((sizeByte+sizeInt) * nxs)) xs
-            mapM_ (writeTree hndl . snd) xs
-            where
-                g i [] = return ()
-                g i ((c,Tree n _ _):xs) = do
-                    hPutByte hndl (ord c)
-                    hPutInt hndl i
-                    g (i + n) xs
+        getSize n [] = []
+        getSize n (x:xs) = (n,x) : getSize (n + treeSize (snd x)) xs
+    
+        f TreeList = hPutInt hndl (length xs) >> mapM_ g sxs
+            where g (i,(c,_)) = hPutByte hndl (ord c) >> hPutInt hndl i
         
---        f (TreeTrie 
-            
-        
+        f TreeTrie = hPutInt hndl (-1) >> mapM_ g ['a'..'z']
+            where g c = case [i | (i,(c2,_)) <- sxs] of
+                            [] -> hPutInt hndl 0
+                            [i] -> hPutInt hndl i
 
 
 -- takes a tree, and the size of each individual element
 -- and computes the size of that subtree
-sizeTree :: Int -> Tree a -> Tree a
-sizeTree n (Tree _ m xs) = Tree (sizeByte + sizeInt + (length xs * n) + f m2) m2 xs
+sizeTree :: Tree -> Tree
+sizeTree (Tree _ mode xs res) = Tree size mode xs2 res
     where
-        m2 = case m of
-                TreeList x -> TreeList (g x)
-                TreeTrie x -> TreeTrie (g x)
-                x -> x
-    
-        g xs = [(a,sizeTree n b) | (a,b) <- xs]
-    
-        f (TreeNone) = 0
-        f (TreeTail s x) = sizeStr s + n
-        f (TreeList xs) = sizeInt + ((sizeByte + sizeInt) * length xs) + h xs
-        f (TreeTrie xs) = (sizeInt * 26) + h xs
+        size = sizeInt + sz + f mode
         
-        h xs = sum [n | (_,Tree n _ _) <- xs]
+        xs2 = [(a,sizeTree b) | (a,b) <- xs]
+        sz = sum $ map (treeSize . snd) xs2
         
+        f TreeTrie = 26 * sizeInt
+        f TreeList = sizeInt + ((sizeByte + sizeInt) * length xs)
         
 
 
 
 -- take in (moduleId, item) -> [(itemId, item, dbitem)]
 saveTexts :: Handle -> [(Int, Item, DBItem)] -> IO [String]
-saveTexts hndl xs = writeTree hndl tree >> return []
+saveTexts hndl xs = do
+        i <- hTellInt hndl
+        hPutInt hndl (i + ntree)
+        writeTree hndl tree
+        mapM_ outItem ids
+        return []
     where
-        tree = sizeTree 8 $ buildTree $ sortBy cmp items
+        outItem (a,b) = hPutInt hndl a >> hPutInt hndl b
+    
+        ntree = treeSize tree
+        tree = sizeTree $ buildTree 0 strs
+        
+        (strs,ids)= unzip $ sortBy cmp items
             where cmp (a,_) (b,_) = compare a b
 
         items = [(map toLower (drop i s), (idn,i)) | (idn,_,DBItem s _) <- xs, i <- [0..length s-1]]
