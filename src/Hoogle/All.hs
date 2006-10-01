f9d0e0f1efcbe1e273dab9f4e12e4b28eaa3fe2f
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-10-01 12:59:09 +0000

    Add a general Hoogle interface, which exports all the public methods

---
+++ src/Hoogle/All.hs
@@ -0,0 +1,10 @@
+
+module Hoogle.All(
+    Query, parseQuery, renderQuery, suggestQuery,
+    DataBase, loadDataBase, newDataBase,
+    searchAll, searchRange
+    ) where
+
+import Hoogle.Query.All
+import Hoogle.DataBase.All
+import Hoogle.Search.All
