3464e6f3cae47eae2d662d17e719c9bd99290509
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-10-02 18:32:55 +0000

    Add a comment on the structure of the types database

--- src/Hoogle/DataBase/Types.hs
+++ src/Hoogle/DataBase/Types.hs
@@ -7,6 +7,25 @@ import General.All
 import Hoogle.Common.All
 import Hoogle.TypeSig.All
 
+{-
+FORMAT
+
+n = number of distinct types
+then n*type-item
+
+type-item =
+    arity = int
+    type structure of each of the components
+
+    count = int
+    count*{id,int*arity} -- the permutations
+
+If two types are equal up to alpha renaming and parameter reordering
+then they are merged into one type value.
+-}
+
+
+
 
 saveTypes :: Handle -> [Item] -> IO [Response]
 saveTypes _ _ = return []
