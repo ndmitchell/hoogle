a1baef5dbc5c18e721fa2a5ab4ea4610ebfeaab1
Neil Mitchell <unknown>
2006-12-22 20:05:05 +0000

    Export a few more things]

--- src/Hoogle/All.hs
+++ src/Hoogle/All.hs
@@ -2,9 +2,11 @@
 module Hoogle.All(
     Query(..), Flag(..), parseQuery, renderQuery, suggestQuery, usefulQuery,
     DataBase, loadDataBase, newDataBase,
-    searchAll, searchRange
+    searchAll, searchRange,
+    Result, renderResult
     ) where
 
 import Hoogle.Query.All
 import Hoogle.DataBase.All
 import Hoogle.Search.All
+import Hoogle.Common.All
