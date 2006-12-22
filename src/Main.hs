e18340a1b99f35a61623e20978d649544030a02c
Neil Mitchell <unknown>
2006-12-22 19:41:39 +0000

    Add a new main file, as a general worker

---
+++ src/Main.hs
@@ -0,0 +1,87 @@
+
+module Main where
+
+import Web.All
+import CmdLine.All
+import Hoogle.All
+import General.All
+
+import Control.Monad
+import Data.Maybe
+import Data.List
+import System.FilePath
+import System.Directory
+
+
+data Origin = Web
+            | CmdLine
+
+
+getQuery :: IO (Origin, String)
+getQuery = do web <- webQuery
+              case web of
+                  Just x -> return (Web, x)
+                  Nothing -> liftM ((,) CmdLine) cmdQuery 
+
+main = do
+    (origin,query) <- getQuery
+    case parseQuery query of
+        Left err -> putStr $ cmdParseError query err
+        Right q -> exec origin q
+
+
+hasFlag :: Query -> [String] -> Bool
+hasFlag q s = isJust $ getFlag q s
+
+
+getFlag :: Query -> [String] -> Maybe String
+getFlag query s = listToMaybe [extra | Flag name extra <- flags query, name `elem` s]
+
+checkFlags :: Query -> [String] -> IO ()
+checkFlags query s = when (not $ null extra) $
+        putStrLn $ "Warning, unrecognised flag" ++ ['s'|length extra > 1] ++ " (ignored): " ++
+                   concat (intersperse ", " extra)
+    where extra = [name | Flag name _ <- flags query] \\ s
+
+
+versionMsg = unlines
+    ["HOOGLE - (C) Neil Mitchell 2004-2007, University of York, UK"
+    ,"Version 4.0 pre"
+    ]
+
+helpMsg = unlines
+    ["Go to the website for help, http://haskell.org/hoogle/"]
+
+
+
+fVersion = ["v","ver","version"]
+fHelp = ["h","help"]
+fConvert = ["c","conv","convert"]
+fOutput = ["o","out","output"]
+
+
+exec :: Origin -> Query -> IO ()
+
+exec CmdLine q | hasFlag q fVersion = putStr versionMsg
+
+exec CmdLine q | hasFlag q fHelp = putStr helpMsg
+
+exec CmdLine q | hasFlag q fConvert = do
+    let input  = fromMaybe "" (getFlag q fConvert)
+        output = fromMaybe (replaceExtension input "hoo") (getFlag q fOutput)
+    checkFlags q (fConvert ++ fOutput)
+    exist <- doesFileExist input
+    if not exist
+        then putStrLn $ "Convert, input file not found: " ++ input
+        else do
+        putStrLn $ "Convert, begining on " ++ input
+        response <- newDataBase input output
+        if anyError response
+            then putStrLn $ "Conversion failed"
+            else putStrLn $ "Conversion successful, created: " ++ output
+    
+
+exec CmdLine q | not $ usefulQuery q = putStr $ "No query given\n" ++ helpMsg
+
+exec _ q = error $ show q
+
