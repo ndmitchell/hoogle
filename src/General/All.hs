6f7c60b64d77dfe4cd6b5e0e881720733e0e4620
Neil Mitchell <unknown>
2006-12-23 22:06:00 +0000

    Add General.HtmlTags module, since Text.Html doesn't actually parse any HTML

--- src/General/All.hs
+++ src/General/All.hs
@@ -1,10 +1,12 @@
 
 module General.All(
     module General.Code, module General.Type,
-    module General.Binary, module General.CGI
+    module General.Binary, module General.CGI,
+    module General.HtmlTags
     ) where
 
 import General.Code
 import General.Type
 import General.Binary
 import General.CGI
+import General.HtmlTags
---
+++ src/General/HtmlTags.hs
@@ -0,0 +1,64 @@
+
+module General.HtmlTags(
+    HtmlTag(..), parseHtmlTags,
+    isOpenTagName, isCloseTagName
+    ) where
+
+import Data.Char
+import Data.List
+
+
+type HtmlAttr = (String,String)
+
+data HtmlTag = OpenTag String [HtmlAttr]
+             | CloseTag String
+             | TextTag String
+             deriving Show
+
+
+parseHtmlTags :: String -> [HtmlTag]
+parseHtmlTags [] = []
+
+parseHtmlTags ('<':'/':xs) = CloseTag tag : parseHtmlTags trail
+    where
+        (tag,rest) = span isAlphaNum xs
+        trail = drop 1 $ dropWhile (/= '>') rest
+
+parseHtmlTags ('<':xs)
+        | "/>" `isPrefixOf` rest2 = res : CloseTag tag : parseHtmlTags (drop 2 rest2)
+        | ">" `isPrefixOf` rest2 = res : parseHtmlTags (drop 1 rest2)
+        | otherwise = res : parseHtmlTags (drop 1 $ dropWhile (/= '>') rest2)
+    where
+        res = OpenTag tag attrs
+        (tag,rest) = span isAlphaNum xs
+        (attrs,rest2) = parseAttributes rest
+
+parseHtmlTags (x:xs) = [TextTag pre | not $ null pre] ++ parseHtmlTags post
+    where (pre,post) = break (== '<') (x:xs)
+
+
+parseAttributes :: String -> ([HtmlAttr], String)
+parseAttributes (x:xs) | isSpace x = parseAttributes xs
+                       | not $ isAlpha x = ([], x:xs)
+                       | otherwise = ((lhs,rhs):attrs, over)
+    where
+        (attrs,over) = parseAttributes (dropWhile isSpace other)
+    
+        (lhs,rest) = span isAlphaNum (x:xs)
+        rest2 = dropWhile isSpace rest
+        (rhs,other) = if "=" `isPrefixOf` rest2 then parseValue (dropWhile isSpace $ tail rest2) else ("", rest2)
+        
+
+parseValue :: String -> (String, String)
+parseValue ('\"':xs) = (a, drop 1 b)
+    where (a,b) = break (== '\"') xs
+parseValue x = span isAlphaNum x
+
+
+
+isCloseTagName name (CloseTag n) = n == name
+isCloseTagName _ _ = False
+
+
+isOpenTagName name (OpenTag n _) = n == name
+isOpenTagName _ _ = False
