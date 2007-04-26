cc88c80dac64b7a4e85c4e2f509b6c6786e29290
Neil Mitchell <unknown>
2007-04-26 21:46:03 +0000

    Rehabilitate the Search module, now works with the new API

--- src/Hoogle/Search/All.hs
+++ src/Hoogle/Search/All.hs
@@ -4,40 +4,42 @@ module Hoogle.Search.All(searchAll, searchRange) where
 import Data.Maybe
 import Data.List
 import Control.Monad
+import General.Code
 
 import Hoogle.DataBase.All
 import Hoogle.Query.All
+import Hoogle.Item.All
+import Hoogle.TypeSig.All
 import Hoogle.Result.All
 
 
-searchAll = undefined
-searchRange = undefined
 
-
-{-
 -- return all the results
-searchAll :: DataBase -> Query -> [Result]
+searchAll :: [DataBase] -> Query -> [Result]
 searchAll databases query = getResults databases query
 
 
 -- should be possible to fast-path certain searches, currently not done
 -- start (0 based), length
-searchRange :: [DataBase] -> Query -> Int -> Int -> IO [Result DataBase]
-searchRange databases query from len = do
-    res <- getResults databases query
-    return $ take len $ drop from res
+searchRange :: [DataBase] -> Query -> Int -> Int -> [Result]
+searchRange databases query from len =
+        take len $ drop from res
+    where
+        res = getResults databases query
 
 
-getResults :: [DataBase] -> Query -> IO [Result DataBase]
-getResults databases query = res >>= return . filterResults query
+getResults :: [DataBase] -> Query -> [Result]
+getResults databases query = filterResults query res
     where
         res = if not (null $ names query) then performTextSearch databases (names query)
               else if isJust (typeSig query) then performTypeSearch databases (fromJust $ typeSig query)
               else error "Search.getResults: Doing a blank search!"
 
-filterResults :: Query -> [Result DataBase] -> [Result DataBase]
+
+-- | Apply the PlusModule and MinusModule modes
+filterResults :: Query -> [Result] -> [Result]
 filterResults q xs = if null actions then xs
-                    else filter (f base actions . modName . fromJust . itemMod . itemResult) xs
+                     else filter (f base actions . modName . itemMod . itemResult) xs
     where
         actions = filter isModule $ scope q
         
@@ -58,46 +60,18 @@ filterResults q xs = if null actions then xs
         doesMatch [] y = True
         doesMatch (x:xs) (y:ys) = x == y && doesMatch xs ys
         doesMatch _ _ = False
--}
 
 
+-- | Put the results in the correct order, by score
+orderResults :: [Result] -> [Result]
+orderResults = map snd . sortBy (compare `on` fst) . map (\x -> (resultScore x, x))
+
+
+-- | Perform a text query
 performTextSearch :: [DataBase] -> [String] -> [Result]
 performTextSearch databases query = concatMap (`searchName` query) databases
 
 
-{-
-
-
-do
-        res <- map (`searchName` query) databases
-        res <- return $ map head $ groupSortBy (compare `on` getItemId) res
-        return $ sortBy priority res
-    where
-        cmpItemId x y = getItemId x `compare` getItemId y
-        eqItemId x y = getItemId x == getItemId y
-        getItemId = fromJust . itemId . itemResult
-
-        priority x y = getStatus x `compare` getStatus y
-        getStatus (Result (Just txt) typ item) =
-            (negate $ length $ textMatch txt
-            ,textElse txt
-            ,textCase txt
-            ,itemPriority $ itemRest item
-            ,(fromJust $ itemName item
-             ,length $ modName $ fromJust $ itemMod item
-             ,modName $ fromJust $ itemMod item
-             )
-            )
--}
-
-{-
-performTypeSearch :: [DataBase] -> TypeSig -> IO [Result DataBase]
-performTypeSearch databases query = do
-        res <- concatMapM (`searchType` query) databases
-        res <- return $ concat $ sortBy cmpResults res
-        return res
-    where
-        cmpResults xs ys = f xs `compare` f ys
-            where
-                f = length . typeDiff . fromJust . typeResult . head
--}
+performTypeSearch :: [DataBase] -> TypeSig -> [Result]
+performTypeSearch databases query = []
+ --     concatMap (`searchType` query) databases
