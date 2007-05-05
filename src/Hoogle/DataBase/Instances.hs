46dcc06d95e3720b0b8b9859716e9f3b18869a56
Neil Mitchell <unknown>
2007-05-05 23:16:13 +0000

    Write up what the Instances code will do, with comments

--- src/Hoogle/DataBase/Instances.hs
+++ src/Hoogle/DataBase/Instances.hs
@@ -1,21 +1,39 @@
 
-module Hoogle.DataBase.Instances(Instances, saveInstances, loadInstances, hasInstance) where
+module Hoogle.DataBase.Instances(
+    Instances, createInstances
+    ) where
 
-import System.IO
-import Hoogle.TextBase.All
-import General.All
+import Hoogle.Item.All
+
+{-
+Instances are a little tricky because they are so different on GHC/Hugs/Haskell.
+
+We supports multi-parameter type classes, but only slightly. The rule is:
+
+Class a b === (Class$0 a, Class$1 b)
+
+Instances are stored as:
+
+Class (Data a b) ==> (Class1 a, Class2 a, Class1 b, Class2 b)
+Class * ==> True
+
+We are able to capture Haskell 98 classes perfectly, and give a reasonable
+interpretation of other things. There are two ways of encoding classes - 'or' and
+'and'. Each definition is one instance of an 'or', but may be multiple 'and's.
+For example:
+
+Class (Data a Bool) is translated to:
+    Class (Data a b) ==> Class_Bool b
+    Class_Bool Bool ==> True
+-}
+
+type ClassName = String
+type DataName  = String
+
+data Instances = Instances (Set.Set ClassName)
+                           (Map.Map (ClassName,DataName) [(ClassName,Int)])
 
 
-data Instances = Instances
+createInstances :: [Item] -> Instances
+createInstances _ = Instances Set.empty Map.empty
 
-
-saveInstances :: Handle -> TextBase -> IO [Response]
-saveInstances hndl tb = return []
-
-
-loadInstances :: Handle -> IO Instances
-loadInstances hndl = return Instances
-
-
-hasInstance :: Instances -> () -> IO Bool
-hasInstance _ _ = return False
