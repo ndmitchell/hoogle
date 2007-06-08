695f1ffc93b6f9189bf35707bb133f4c41033964
Neil Mitchell <unknown>
2007-06-08 20:41:31 +0000

    Add foo -> Foo suggestions to the query thing

--- src/Hoogle/Query/Suggest.hs
+++ src/Hoogle/Query/Suggest.hs
@@ -3,8 +3,12 @@ module Hoogle.Query.Suggest(suggestQuery) where
 
 import General.All
 import Data.List
+import Data.Char
+import Data.Generics.UniplateOn
 import Hoogle.DataBase.All
+import Hoogle.TypeSig.All
 import Hoogle.Query.Type
+import Hoogle.Query.Render
 
 
 suggestQuery :: DataBase -> Query -> Maybe TagStr
@@ -13,31 +17,31 @@ suggestQuery db q | "google" `elem` names q = Just $ Tags [TagHyperlink "http://
 suggestQuery db q | any f (names q) = Just $ Str "Can't think of anything more interesting to search for?"
     where f x = length x == 6 && "oogle" `isSuffixOf` x
 
+suggestQuery db q | q /= q2 = Just $ suggestSearch q2
+    where
+        q2 = transformQueryType f q
+
+        f (TVar (x:xs)) | not $ null xs = TLit ('\0' : toUpper x : xs)
+        f x = x
+
 suggestQuery _ _ = Nothing
 
-{-
 
-hoogleSuggest (Search _ (SearchType (c,t))) |
-        any dubiousVar (allTVar t) = Just $ Tags
-        [Str "Did you mean: ", Tag "a" (Tags $ f $ showConType (c, mapUnbound safeVar t))]
+
+
+suggestSearch :: Query -> TagStr
+suggestSearch x = TagHyperlink (showUrl x) (showText x)
     where
-        dubiousVar x = length x > 1
-        safeVar x | dubiousVar x = TLit $ '{' : toUpper (head x) : tail x ++ "}"
-                  | otherwise = TVar x
-        
-        f xs = Str a : (if null b then [] else Tag "b" (Str c) : f (safeTail d))
+        showUrl = showTag . renderQuery . transformQueryType f
+        f (TLit ('\0':xs)) = TLit xs
+        f x = x
+
+        showText = Tags . zeros . showTag . renderQuery . transformQueryType g
+        g (TLit ('\0':xs)) = TLit ('\0':xs ++ "\0")
+        g x = x
+
+        zeros xs | null b = [Str a]
+                 | otherwise = Str a : TagBold (Str c) : zeros (drop 1 d)
             where
-                (a,b) = break (== '{') xs
-                (c,d) = break (== '}') (tail b)
-                
-                safeTail [] = []
-                safeTail (x:xs) = xs
-        
-querySuggest (Search _ (SearchName xs)) | xs == "google" =
-        Just $ Tags [Tag "a" (Str "http://www.google.com/"), Str " rocks!"]
-
-querySuggest (Search _ (SearchName (x:xs))) | xs == "oogle" =
-        Just $ Str "Can't think of anything more interesting to search for?"
-
-querySuggest _ = Nothing
--}
+                (a,b) = break (== '\0') xs
+                (c,d) = break (== '\0') (tail b)
