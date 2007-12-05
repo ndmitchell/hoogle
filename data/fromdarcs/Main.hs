d8f246ffdfc7baff40412b6a5810af91e8528cd7
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2007-12-05 10:52:20 +0000

    More changes to the haddock generation code, now generatres the haddock files

--- data/fromdarcs/Main.hs
+++ data/fromdarcs/Main.hs
@@ -5,7 +5,9 @@ import System.Cmd
 import System.Exit
 import System.Directory
 import System.FilePath
+import System.IO
 import Control.Monad
+import Data.List
 
 
 packages = ["http://darcs.haskell.org/packages/base/"
@@ -28,9 +30,13 @@ generate url = do
         else system_ $ "darcs get --partial " ++ url ++ " --repodir=" ++ dir
 
     setCurrentDirectory $ "grab" </> name
+    removeFile_ "Setup.hs"
+    removeFile_ "Setup.lhs"
+    writeFile "Setup.hs" "import Distribution.Simple; main = defaultMain"
+    when (name == "base") $ fixupBaseCabal
     system_ $ "ghc -i --make Setup"
     system_ $ "setup configure"
-    system_ $ "setup haddock --hoogle -v"
+    system_ $ "setup haddock --hoogle"
 
     setCurrentDirectory "../.."
 
@@ -40,3 +46,20 @@ system_ x = do
     res <- system x
     when (res /= ExitSuccess) $
         error "Command failed"
+
+removeFile_ x = do
+    b <- doesFileExist x
+    when b $ removeFile x
+
+readFile' x = do
+    h <- openFile x ReadMode
+    s <- hGetContents h
+    () <- length s `seq` return ()
+    hClose h
+    return s
+
+fixupBaseCabal = do
+    x <- readFile' "base.cabal"
+    let (pre,post) = break ("impl(ghc)" `isInfixOf`) $ lines x
+        post2 = drop 1 $ dropWhile ('}' `notElem`) post
+    writeFile "base.cabal" $ unlines $ pre ++ post2
