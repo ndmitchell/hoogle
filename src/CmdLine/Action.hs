3cd23020351ab2ff20426f2bd9974270ee9b37d2
Neil Mitchell <unknown>
2008-05-27 22:32:35 +0000

    Start fleshing out actionCmdLine

--- src/CmdLine/Action.hs
+++ src/CmdLine/Action.hs
@@ -1,12 +1,18 @@
 
-module CmdLine.Action(cmdParseError) where
+module CmdLine.Action(actionCmdLine) where
 
+import CmdLine.Query
 import Text.ParserCombinators.Parsec
+import System.Exit
 
 
-cmdParseError :: String -> ParseError -> String
-cmdParseError orig err = unlines
-    ["Parse error:"
-    ,orig
-    ,replicate (sourceColumn (errorPos err) - 1) ' ' ++ "^"
-    ] ++ show err
+actionCmdLine :: CmdQuery -> IO ()
+actionCmdLine CmdQuery{queryText = text, query = Left err} = do
+    putStrLn $ msg ++ show err
+    exitFailure
+    where
+        msg = unlines ["Parse error:", "  " ++ text,
+                        replicate (sourceColumn (errorPos err) + 1) ' ' ++ "^"]
+
+
+actionCmdLine _ = error "todo"
