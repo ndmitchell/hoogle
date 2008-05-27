42d80a33b7d8002300803d51dc90d80d91d01e2d
Neil Mitchell <unknown>
2008-05-27 21:43:57 +0000

    Revamp CmdLine.Query

--- src/CmdLine/Query.hs
+++ src/CmdLine/Query.hs
@@ -1,14 +1,50 @@
 
 module CmdLine.Query(cmdQuery) where
 
-import System.Environment
-import Data.List
+import Control.Monad
 import Data.Char
+import Data.List
+import System.Environment
+import Text.ParserCombinators.Parsec
+
+import CmdLine.Flag
+import General.CGI
+import Hoogle.Query.All
 
 
-cmdQuery :: IO String
-cmdQuery = do s <- getArgs
-              return $ concat $ intersperse " " $ map f s
+-- | Left (query text, error message), null query text = no query given
+--   Right (query, command line flags, unparsable/unsupported flags)
+cmdQuery :: IO (Either (String,ParseError) (Query, [CmdFlag], [String]))
+cmdQuery = do
+    r <- cgiArgs
+    case r of
+        Just y -> return $ cmdQueryCGI y
+        Nothing -> liftM cmdQueryArgs getArgs
+
+
+cmdQueryCGI :: [(String,String)] -> Either (String,ParseError) (Query, [CmdFlag], [String])
+cmdQueryCGI [("",x)] = cmdQueryCGI [("q",x)]
+cmdQueryCGI xs = case parseQuery str of
+    Left err -> Left (str,err)
+    Right res -> let (a1,b1) = flagsWebQuery $ queryArgs res
+                     (a2,b2) = flagsWebArgs notq
+                 in Right (res, Web : delete Web (a1++a2), b1++b2)
     where
-        f x = q ++ x ++ q
-            where q = ['\"' | any isSpace x]
+        (q,notq) = partition ((==) "q" . fst) xs
+        str = unwords $ map snd q
+
+
+cmdQueryArgs :: [String] -> Either (String,ParseError) (Query, [CmdFlag], [String])
+cmdQueryArgs xs = case parseCmdLineQuery xs of
+    Left err -> Left (unwords $ map quote xs, err)
+    Right res -> let (a,b) = flagsCmdLine $ queryArgs res
+                 in Right (res, a, b)
+
+
+quote :: String -> String
+quote x | any isSpace x = "\"" ++ x ++ "\""
+        | otherwise = x
+
+
+queryArgs :: Query -> [(String,String)]
+queryArgs q = [(a,b) | Flag a b <- flags q]
