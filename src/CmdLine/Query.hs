5da2c0248ccff3fc35592f6740e4a37a4d2fc351
Neil Mitchell <unknown>
2008-05-27 22:13:27 +0000

    Change the CmdLine.Query to return more full information on the command line

--- src/CmdLine/Query.hs
+++ src/CmdLine/Query.hs
@@ -1,5 +1,18 @@
+{-|
+    Parse a query, that may have come from either a CGI variable
+    or the command line arguments.
 
-module CmdLine.Query(cmdQuery) where
+    Need to return the following pieces of information:
+    
+    * Was there a query, or was nothing entered
+    
+    * Are you wanting to operate in Web mode or Command Line mode. Adding a
+    Web parameter to Command Line gives you Web mode.
+
+    * Which flags were specified, and which were erroneous.
+-}
+
+module CmdLine.Query(cmdQuery, CmdQuery(..)) where
 
 import Control.Monad
 import Data.Char
@@ -12,9 +25,18 @@ import General.CGI
 import Hoogle.Query.All
 
 
+data CmdQuery = CmdQuery {
+    queryWeb :: Bool, -- ^ Are you operating from the web (via CGI)
+    queryText :: String, -- ^ The string the user entered, @""@ for no string
+    query :: Either ParseError Query, -- ^ The actual query
+    queryFlags :: [CmdFlag], -- ^ The flags from the query
+    queryBadFlags :: [String] -- ^ The bad flags
+    }
+
+
 -- | Left (query text, error message), null query text = no query given
 --   Right (query, command line flags, unparsable/unsupported flags)
-cmdQuery :: IO (Either (String,ParseError) (Query, [CmdFlag], [String]))
+cmdQuery :: IO CmdQuery
 cmdQuery = do
     r <- cgiArgs
     case r of
@@ -22,23 +44,24 @@ cmdQuery = do
         Nothing -> liftM cmdQueryArgs getArgs
 
 
-cmdQueryCGI :: [(String,String)] -> Either (String,ParseError) (Query, [CmdFlag], [String])
+cmdQueryCGI :: [(String,String)] -> CmdQuery
 cmdQueryCGI [("",x)] = cmdQueryCGI [("q",x)]
 cmdQueryCGI xs = case parseQuery str of
-    Left err -> Left (str,err)
-    Right res -> let (a1,b1) = flagsWebQuery $ queryArgs res
-                     (a2,b2) = flagsWebArgs notq
-                 in Right (res, Web : delete Web (a1++a2), b1++b2)
+    Left err -> CmdQuery True str (Left err) flags1 bad1
+    Right res -> let (flags2,bad2) = flagsWebQuery $ queryArgs res
+                 in CmdQuery True str (Right res) (flags2++flags1) (bad2++bad1)
     where
         (q,notq) = partition ((==) "q" . fst) xs
+        (flags1,bad1) = flagsWebArgs notq
         str = unwords $ map snd q
 
 
-cmdQueryArgs :: [String] -> Either (String,ParseError) (Query, [CmdFlag], [String])
+cmdQueryArgs :: [String] -> CmdQuery
 cmdQueryArgs xs = case parseCmdLineQuery xs of
-    Left err -> Left (unwords $ map quote xs, err)
-    Right res -> let (a,b) = flagsCmdLine $ queryArgs res
-                 in Right (res, a, b)
+    Left err -> CmdQuery False orig (Left err) [] []
+    Right res -> let (flags,bad) = flagsCmdLine $ queryArgs res
+                 in CmdQuery (Web `elem` flags) orig (Right res) flags bad
+    where orig = unwords $ map quote xs
 
 
 quote :: String -> String
