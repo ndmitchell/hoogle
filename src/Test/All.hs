be08895d6fde4ae14b73ae06dd4bcdc7dd1e3897
Neil Mitchell <unknown>
2008-06-16 09:47:36 +0000

    Add the TextBase parsing tests to the general list

--- src/Test/All.hs
+++ src/Test/All.hs
@@ -3,9 +3,11 @@ module Test.All(test) where
 
 import Test.Parse_TypeSig
 import Test.Parse_Query
+import Test.Parse_TextBase
 
 
 test :: IO ()
 test = print $ do
     parse_TypeSig
     parse_Query
+    parse_TextBase
