a01e95208d42b49aafcbc5766bffc56d7d13b95b
Neil Mitchell <unknown>
2008-06-16 09:47:23 +0000

    Add a test for the TextBase parser

---
+++ src/Test/Parse_TextBase.hs
@@ -0,0 +1,44 @@
+-- TODO Flesh this out as a proper test
+
+module Test.Parse_TextBase(parse_TextBase) where
+
+import Test.General
+import Hoogle.TextBase.All
+import Hoogle.TypeSig.All
+
+
+typ x = case parseTypeSig x of
+            Left err -> error $ "parse_TextBase, failed to parse type signature: " ++ show x
+            Right y -> y
+
+parse_TextBase = do
+    let (===) = parseTest parseTextBaseString
+
+    "" === []
+    "-- foo" === []
+
+    "module Foo.Bar.String" === [ItemModule ["Foo","Bar","String"]]
+    "module Test" === [ItemModule ["Test"]]
+
+    "class Foo a" === [ItemClass $ typ "Foo a"]
+    "class Foo a b" === [ItemClass $ typ "Foo a b"]
+
+    "foo :: Bool" === [ItemFunc "foo" $ typ "Bool"]
+    "(++) :: Int" === [ItemFunc "++" $ typ "Int"]
+    "++ :: Int" === [ItemFunc "++" $ typ "Int"]
+    "@@ :: Int" === [ItemFunc "@@" $ typ "Int"]
+
+    "type Bar a = Foo [a]" === [ItemAlias (typ "Bar a") (typ "Foo [a]")]
+
+    "data Foo a" === [ItemData DataKeyword $ typ "Foo a"]
+    "newtype Foo a" === [ItemData NewTypeKeyword $ typ "Foo a"]
+
+    "instance Eq a => Eq [a]" === [ItemInstance $ typ "Eq a => Eq [a]"]
+
+    "@keyword ~" === [ItemAttribute "keyword" "~"]
+    "@keyword ->" === [ItemAttribute "keyword" "->"]
+    "@keyword module" === [ItemAttribute "keyword" "module"]
+
+    "@package neil" === [ItemAttribute "package" "neil"]
+    "@foo bar" === [ItemAttribute "foo" "bar"]
+    "@debug" === [ItemAttribute "debug" ""]
