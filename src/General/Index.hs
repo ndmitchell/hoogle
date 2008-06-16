d5a81c93659ca10f5b2ed88f3c89d613780c215e
Neil Mitchell <unknown>
2008-06-16 06:17:13 +0000

    Change Int -> Id in General.Index

--- src/General/Index.hs
+++ src/General/Index.hs
@@ -1,5 +1,6 @@
 
 module General.Index(
+    Id,
     Index, newIndex,
     Lookup, newLookup, lookupKey, lookupVal
     ) where
@@ -7,13 +8,16 @@ module General.Index(
 import Data.Array
 
 
-newtype Index a = Index (Array Int a)
+type Id = Int
 
+newtype Index a = Index (Array Id a)
+
+-- | Items will obtain the Id's 0..length-1
 newIndex :: [a] -> Index a
 newIndex xs = Index $ listArray (0,length xs - 1) xs
 
 
-data Lookup a = Lookup {lookupKey :: Int, lookupVal :: a}
+data Lookup a = Lookup {lookupKey :: Id, lookupVal :: a}
 
-newLookup :: Int -> Index a -> Lookup a
+newLookup :: Id -> Index a -> Lookup a
 newLookup i (Index xs) = Lookup i (xs ! i)
