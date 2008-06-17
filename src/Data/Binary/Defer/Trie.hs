659d21281c61ca94e7834d0c585cfa050eee9f50
Neil Mitchell <unknown>
2008-06-17 17:52:00 +0000

    Add a Trie

---
+++ src/Data/Binary/Defer/Trie.hs
@@ -0,0 +1,44 @@
+
+module Data.Binary.Defer.Trie(
+    Trie, newTrie, lookupTrie
+    ) where
+
+import Data.Binary.Defer
+import Data.List
+import Data.Maybe
+import General.Code
+
+
+-- Keep simple first, can move to an Array later if necessary
+data Trie a = Trie (Maybe a) [(Char,Trie a)]
+              deriving Eq
+
+
+newTrie :: [(String,a)] -> Trie a
+newTrie = newTrieOrdered . sortBy (compare `on` fst)
+
+
+newTrieOrdered :: [(String,a)] -> Trie a
+newTrieOrdered xs = Trie
+        (fmap snd $ listToMaybe as)
+        (map f $ groupBy ((==) `on` (head . fst)) bs)
+    where
+        (as,bs) = span (null . fst) xs
+        f xs = (head $ fst $ head xs, newTrieOrdered [(a,b) | (_:a,b) <- xs])
+
+
+lookupTrie :: String -> Trie a -> Maybe a
+lookupTrie [] (Trie a b) = a
+lookupTrie (x:xs) (Trie a b) = lookup x b >>= lookupTrie xs
+
+
+instance BinaryDefer a => BinaryDefer (Trie a) where
+    put (Trie a b) = put a >> put b
+    get = get2 Trie
+
+instance Show a => Show (Trie a) where
+    show = unlines . f ""
+        where
+            f str (Trie res xs) =
+                [show str ++ " = " ++ show r | Just r <- [res]] ++
+                concat [f (str ++ [c]) t | (c,t) <- xs]
