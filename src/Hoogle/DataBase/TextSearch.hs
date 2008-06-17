93f6f5288237670af2e826064b218654ef78a7cf
Neil Mitchell <unknown>
2008-06-17 21:55:38 +0000

    Implement the TextSearch type

--- src/Hoogle/DataBase/TextSearch.hs
+++ src/Hoogle/DataBase/TextSearch.hs
@@ -2,13 +2,83 @@
 module Hoogle.DataBase.TextSearch where
 
 import Data.Binary.Defer
+import Data.Binary.Defer.Trie
+import Data.Binary.Defer.Chunk
+import Data.Binary.Defer.Index
+import Data.Char
+import Data.List
+import General.Code
+import Hoogle.Item.All
+import Hoogle.TextBase.All
 
-data TextSearch = TextSearch deriving Show
 
-createTextSearch :: a -> TextSearch
-createTextSearch _ = TextSearch
+data TextSearch = TextSearch (Trie (Int,Int)) (Chunk (Int,Lookup Entry))
+                  deriving Show
 
 instance BinaryDefer TextSearch where
-    put _ = return ()
-    get = return TextSearch
+    put (TextSearch a b) = put a >> put b
+    get = get2 TextSearch
 
+{-
+TRIE data structure
+
+Given the functions "map" and "pm" we would generate:
+
+Trie:
+"ap"   (0,0)
+"m"    (1,2)
+"ma"   (2,2)  
+"map"  (2,2)
+"p"    (3,4)
+"pm"   (4,4)
+
+Chunk:
+0      (1,[map])
+1      (1,[pm])
+2      (0,[map])
+3      (2,[map])
+4      (0,[pm])
+
+There will be one trie entry per unique substring.
+There will be one chunk entry per prefix.
+Both are sorted by the string they represent.
+
+[item] is the id of the item.
+
+Given: x (from,to) (start,id)
+
+All items in the list ['from'..'to'] have the string 'x'
+starting at position 'start' in the name given by 'id'.
+
+Note the list has been sorted, and all prefixes appear.
+-}
+
+
+createTextSearch :: [(TextItem, Maybe Entry)] -> TextSearch
+createTextSearch xs = TextSearch
+        (newTrie $ f sub (zip [0..] pre))
+        (newChunk $ map snd pre)
+    where
+        ys = extractText xs
+        sub = map head $ group $ sort $ concatMap (substrs . fst) ys
+        pre = sortBy (compare `on` fst)
+                  [(p,(i,e)) | (s,e) <- ys, (i,p) <- zip [0..] $ prefixes s]
+
+        f :: [String] -> [(Int,(String,a))] -> [(String,(Int,Int))]
+        f [] _ = []
+        f (x:xs) ys = (x,(s,s+n-1)) : f xs ys2
+            where
+                s = fst $ head ys
+                n = length eq + length pr
+                (eq,ys2) = span ((==) x . fst . snd) ys
+                pr = takeWhile (isPrefixOf x . fst . snd) ys2
+
+
+extractText :: [(TextItem, Maybe Entry)] -> [(String, Lookup Entry)]
+extractText xs = [(map toLower s, newLookup $ entryId e)
+                 |(_, Just e) <- xs, Focus s <- entryText e]
+
+
+substrs, prefixes :: [a] -> [[a]]
+substrs = concatMap (tail . inits) . prefixes
+prefixes = init . tails
