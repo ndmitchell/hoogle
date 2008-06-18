669681e5066143ac05e759de65b93962bf0595ab
Neil Mitchell <unknown>
2008-06-18 15:52:31 +0000

    Start to implement some of the data structures for Suggest

--- src/Hoogle/DataBase/Suggest.hs
+++ src/Hoogle/DataBase/Suggest.hs
@@ -2,12 +2,32 @@
 module Hoogle.DataBase.Suggest where
 
 import Data.Binary.Defer
+import Data.Binary.Defer.Trie
 
-data Suggest = Suggest deriving Show
 
-createSuggest :: a -> Suggest
-createSuggest _ = Suggest
+newtype Suggest = Suggest (Trie [SuggestItem]) deriving Show
+
+data SuggestItem = Ctor String -- constructor (and who the type is)
+                 | Data String [Int] -- data type, name (case correct), and possible kinds
+                 | Class String [Int] -- class, name (case correct), kinds
+                   deriving Show
+
+
+instance BinaryDefer SuggestItem where
+    put (Ctor x) = putByte 0 >> put x
+    put (Data x y) = putByte 1 >> put x >> put y
+    put (Class x y) = putByte 2 >> put x >> put y
+
+    get = do i <- getByte
+             case i of
+                0 -> get1 Ctor
+                1 -> get2 Data
+                2 -> get2 Class
 
 instance BinaryDefer Suggest where
-    put _ = return ()
-    get = return Suggest
+    put (Suggest x) = put x
+    get = get1 Suggest
+
+
+createSuggest :: a -> Suggest
+createSuggest _ = Suggest $ newTrie []
