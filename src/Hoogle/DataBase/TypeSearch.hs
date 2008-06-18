21199bd63bab26991cf954a3e84624bbe719b6ec
Neil Mitchell <unknown>
2008-06-18 21:14:17 +0000

    Add stubs for a search in TypeSearch

--- src/Hoogle/DataBase/TypeSearch.hs
+++ src/Hoogle/DataBase/TypeSearch.hs
@@ -2,12 +2,34 @@
 module Hoogle.DataBase.TypeSearch where
 
 import Data.Binary.Defer
+import Data.Binary.Defer.Index
+import Hoogle.DataBase.Item
+import Hoogle.TypeSig.All
+
 
 data TypeSearch = TypeSearch deriving Show
-
-createTypeSearch :: a -> TypeSearch
-createTypeSearch _ = TypeSearch
 
 instance BinaryDefer TypeSearch where
     put _ = return ()
     get = return TypeSearch
+
+
+---------------------------------------------------------------------
+-- CREATION
+
+createTypeSearch :: a -> TypeSearch
+createTypeSearch _ = TypeSearch
+
+
+---------------------------------------------------------------------
+-- SEARCHING
+
+data TypeScore = TypeScore
+                 deriving (Eq,Ord)
+
+instance Show TypeScore where
+    show TypeScore = "_"
+
+
+searchTypeSearch :: TypeSearch -> Index Entry -> TypeSig -> [(Entry,EntryView,TypeScore)]
+searchTypeSearch _ _ _ = []
