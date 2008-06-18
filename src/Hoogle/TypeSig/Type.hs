29824a1fe2d8e0391de5fa1db0d6c9934a892739
Neil Mitchell <unknown>
2008-06-18 16:49:09 +0000

    Update the TLit comment regarding tuples

--- src/Hoogle/TypeSig/Type.hs
+++ src/Hoogle/TypeSig/Type.hs
@@ -17,7 +17,7 @@ type Constraint = [Type]
 -- TApp (TLit "->") xs == TFun xs
 -- TFun is the top level, below than is TLit
 data Type = TApp Type [Type] -- a list of types, first one being the constructor
-          | TLit String -- bound variables, Maybe, ":", "," (tuple), "->" (function)
+          | TLit String -- bound variables, Maybe, ":", "(,)", "(,,)" (tuple), "->" (function)
           | TVar String -- unbound variables, "a"
           | TFun [Type] -- TODO: eliminate this constructor
           deriving Eq
