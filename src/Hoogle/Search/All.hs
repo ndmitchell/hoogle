a1ae931236ab432eec4c17579c3fd827e9bab127
Neil Mitchell <unknown>
2008-06-18 15:26:03 +0000

    Major changes to the Results side of things, can now integrate multiple searches from different queries and databases at once

--- src/Hoogle/Search/All.hs
+++ src/Hoogle/Search/All.hs
@@ -15,7 +15,7 @@ import Hoogle.TypeSig.All
 
 -- return all the results
 searchAll :: [DataBase] -> Query -> [Result]
-searchAll databases query = getResults databases query
+searchAll databases query = getResults query databases
 
 
 -- should be possible to fast-path certain searches, currently not done
@@ -23,23 +23,21 @@ searchRange :: (Int,Int) -> [DataBase] -> Query -> [Result]
 searchRange (from,to) databases query =
         take (1 + to-from) $ drop from res
     where
-        res = getResults databases query
+        res = getResults query databases
 
 
-getResults :: [DataBase] -> Query -> [Result]
-getResults databases query = orderResults $ filterResults query res
+getResults :: Query -> [DataBase] -> [Result]
+getResults query = mergeDataBaseResults . map (mergeQueryResults query . f)
     where
-        res = if not (null $ names query) then performTextSearch databases (names query)
-              else if isJust (typeSig query) then performTypeSearch databases (fromJust $ typeSig query)
-              else error "Search.getResults: Doing a blank search!"
+        f d = [typeSearch d q | Just q <- [typeSig query]] ++
+              map (textSearch d) (names query)
 
 
 -- | Perform a text query
-performTextSearch :: [DataBase] -> [String] -> [Result]
-performTextSearch databases (query:_) = concatMap f databases
-    where
-        f db = [Result e (entryParents db e) [v] [TextScore s] | (e,v,s) <- searchText db query]
+textSearch :: DataBase -> String -> [Result]
+textSearch db query = [Result e (entryParents db e) [v] [TextScore s]
+                      | (e,v,s) <- searchText db query]
 
 
-performTypeSearch :: [DataBase] -> TypeSig -> [Result]
-performTypeSearch databases query = [] -- TODO: concatMap (`searchType` query) databases
+typeSearch :: DataBase -> TypeSig -> [Result]
+typeSearch db query = []
--- src/Hoogle/Search/Results.hs
+++ src/Hoogle/Search/Results.hs
@@ -1,10 +1,12 @@
 
 module Hoogle.Search.Results(
-    filterResults, orderResults
+    mergeDataBaseResults, mergeQueryResults
     ) where
 
 import Data.Char
 import Data.List
+import qualified Data.IntSet as IntSet
+import qualified Data.IntMap as IntMap
 import General.All
 
 import Hoogle.DataBase.All
@@ -12,12 +14,65 @@ import Hoogle.Query.All
 import Hoogle.Search.Result
 
 
+type ResultScore = (Result, RScore)
+
+data RScore = RScore [Score] Int String
+              deriving (Eq,Ord)
+
+
+
+mergeDataBaseResults :: [[ResultScore]] -> [Result]
+mergeDataBaseResults = map fst . fold [] mergeSnd
+
+
+mergeSnd xs [] = xs
+mergeSnd [] ys = ys
+mergeSnd (x:xs) (y:ys) | snd x <= snd y = x : mergeSnd xs (y:ys)
+                       | otherwise = y : mergeSnd (x:xs) ys
+
+
+fold :: a -> (a -> a -> a) -> [a] -> a
+fold x f [] = x
+fold x f xs = fold1 f xs
+
+
+fold1 :: (a -> a -> a) -> [a] -> a
+fold1 f [x] = x
+fold1 f xs = f (fold1 f a) (fold1 f b)
+    where (a,b) = halves xs
+
+
+halves [] = ([], [])
+halves (x:xs) = (x:b,a)
+    where (a,b) = halves xs
+
+
+-- may be duplicates, eliminate them and order them
+mergeQueryResults :: Query -> [[Result]] -> [ResultScore]
+mergeQueryResults q = filterResults q . joinResults . map (nubResults . orderResults)
+
+
+-- join the results of multiple searches
+joinResults :: [[ResultScore]] -> [ResultScore]
+joinResults [] = []
+joinResults [x] = x
+joinResults xs = orderResults $ f $ map asSet xs
+    where
+        asSet = IntMap.fromList . map (\(x,_) -> (entryId $ resultEntry x, x))
+
+        f [x] = IntMap.elems x
+        f (x:y:zs) = f (IntMap.intersectionWith g x y : zs)
+        g r1 r2 = r1{resultScore = sort $ resultScore r1 ++ resultScore r2
+                    ,resultView = resultView r1 ++ resultView r2}
+
+
+
 -- | Apply the PlusModule, MinusModule and MinusPackage modes
-filterResults :: Query -> [Result] -> [Result]
+filterResults :: Query -> [ResultScore] -> [ResultScore]
 filterResults q = f mods correctModule . f pkgs correctPackage
     where
         f [] act = id
-        f xs act = filter (maybe True (act xs) . resultModPkg)
+        f xs act = filter (maybe True (act xs) . resultModPkg . fst)
 
         mods = filter (\x -> isPlusModule x || isMinusModule x) $ scope q
         pkgs = [x | MinusPackage x <- scope q]
@@ -46,10 +101,22 @@ correctModule mods = f base mods . moduleName . fst
 
 
 -- | Put the results in the correct order, by score
-orderResults :: [Result] -> [Result]
-orderResults = map snd . sortBy (compare `on` fst) . map (\x -> (f x, x))
+orderResults :: [Result] -> [ResultScore]
+orderResults = sortScores . map (\x -> (x, f x))
     where
-        f r = (resultScore r
-              ,maybe 0 (length . moduleName . fst) $ resultModPkg r
-              ,map toLower $ entryName $ resultEntry r)
+        f r = RScore (resultScore r)
+                     (maybe 0 (length . moduleName . fst) $ resultModPkg r)
+                     (map toLower $ entryName $ resultEntry r)
+
+sortScores x = sortBy (compare `on` snd) x
+
+
+-- nub everything having the same entryId
+nubResults :: [ResultScore] -> [ResultScore]
+nubResults = f IntSet.empty
+    where
+        f s [] = []
+        f s (r:rs) | i `IntSet.member` s = f s rs
+                   | otherwise = r : f (IntSet.insert i s) rs
+            where i = entryId $ resultEntry $ fst r
 
