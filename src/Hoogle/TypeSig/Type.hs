8df0d3df420d4313dae8a53daa79fdbd6182faf8
Neil Mitchell <unknown>
2008-06-18 18:57:42 +0000

    Remove inaccurate comments in TypeSig

--- src/Hoogle/TypeSig/Type.hs
+++ src/Hoogle/TypeSig/Type.hs
@@ -14,8 +14,6 @@ data TypeSig = TypeSig Constraint Type
 type Constraint = [Type]
 
 
--- TApp (TLit "->") xs == TFun xs
--- TFun is the top level, below than is TLit
 data Type = TApp Type [Type] -- a list of types, first one being the constructor
           | TLit String -- bound variables, Maybe, ":", "(,)", "(,,)" (tuple)
           | TVar String -- unbound variables, "a"
