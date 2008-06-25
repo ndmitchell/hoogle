0c79002ebf34ecff2a646dd21ddf795457de1ec0
Neil Mitchell <unknown>
2008-06-25 23:13:11 +0000

    Add more sorting functions to Data.Key

--- src/Data/Key.hs
+++ src/Data/Key.hs
@@ -1,7 +1,7 @@
 
 module Data.Key(
-    Key, toKey, fromKey,
-    sortKeys, sortFst
+    Key, keyPair, toKey, fromKey,
+    sortKeys, sortWith, sortOn, sortFst
     ) where
 
 import Data.List
@@ -18,6 +18,10 @@ instance Ord k => Ord (Key k v) where
     compare (Key k1 v1) (Key k2 v2) = compare k1 k2
 
 
+keyPair :: k -> v -> Key k v
+keyPair = Key
+
+
 toKey :: (v -> k) -> v -> Key k v
 toKey f v = Key (f v) v
 
@@ -26,9 +30,19 @@ fromKey :: Key k v -> v
 fromKey (Key k v) = v
 
 
-sortKeys :: Ord k => (v -> k) -> [v] -> [v]
-sortKeys f = map fromKey . sort . map (toKey f)
+sortKeys :: Ord k => [Key k v] -> [v]
+sortKeys = map fromKey . sort
+
+
+-- | @sort f == sortBy (compare `on` f)@
+--   but @f@ will only be applied to each element once
+sortWith :: Ord k => (v -> k) -> [v] -> [v]
+sortWith f = sortKeys . map (toKey f)
+
+
+sortOn :: Ord k => (v -> k) -> [v] -> [v]
+sortOn f = sortBy (compare `on` f)
 
 
 sortFst :: Ord k => [(k,v)] -> [(k,v)]
-sortFst = sortBy (compare `on` fst)
+sortFst = sortOn fst
