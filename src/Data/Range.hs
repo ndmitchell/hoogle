30291fc6e1244264359f30748315fe60c66eda05
Neil Mitchell <unknown>
2008-06-25 12:18:54 +0000

    Add a Data.Range type

---
+++ src/Data/Range.hs
@@ -0,0 +1,38 @@
+{-|
+    Invariants: Count >= 0, End >= Start, Start >= 0
+
+    Start/End are 0-based indicies
+
+    Count may be maxBound::Int!
+
+    The rangeStartCount and rangeStartEnd methods ensure the invariants,
+    modifying the range if necessary
+-}
+
+module Data.Range(
+    Range,
+    rangeStart, rangeCount, rangeEnd,
+    rangeStartCount, rangeStartEnd,
+    listRange
+    ) where
+
+
+data Range = Range {rangeStart :: Int, rangeCount :: Int}
+
+nat = max (0::Int)
+
+rangeEnd :: Range -> Int
+rangeEnd r = rangeStart r + rangeCount r - 1
+
+
+rangeStartCount :: Int -> Int -> Range
+rangeStartCount start count = Range (nat start) (nat count)
+
+
+rangeStartEnd :: Int -> Int -> Range
+rangeStartEnd start end = Range start2 (nat $ end - start2 + 1)
+    where start2 = nat start
+
+
+listRange :: Range -> [a] -> [a]
+listRange r = take (rangeCount r) . drop (rangeStart r)
