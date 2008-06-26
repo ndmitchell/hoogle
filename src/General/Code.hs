c51381ef4c5b8b8233fbc82a41a504a63854149c
Neil Mitchell <unknown>
2008-06-26 15:24:51 +0000

    Make General.Code export lots of commonly accessed modules

--- src/General/Code.hs
+++ src/General/Code.hs
@@ -1,6 +1,19 @@
 
 module General.Code(
-    module General.Util
+    module General.Util,
+    module Control.Arrow, module Control.Monad,
+    module Data.Char, module Data.Maybe, module Data.List,
+    module System.Cmd, module System.Directory, module System.Exit, module System.IO
     ) where
 
 import General.Util
+
+import Control.Arrow
+import Control.Monad
+import Data.Char
+import Data.Maybe
+import Data.List
+import System.Cmd
+import System.Directory
+import System.Exit
+import System.IO
