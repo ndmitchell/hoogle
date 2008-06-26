a05e4ffc2b2a407ecf7c08f4a4ac474dc54d6c8d
Neil Mitchell <unknown>
2008-06-26 15:18:08 +0000

    Remove General.Binary, is superceeded by Data.Binary.Raw

--- src/General/All.hs
+++ src/General/All.hs
@@ -1,11 +1,10 @@
 
 module General.All(
     module General.Code,
-    module General.Binary, module General.CGI,
+    module General.CGI,
     module General.HtmlTags
     ) where
 
 import General.Code
-import General.Binary
 import General.CGI
 import General.HtmlTags
