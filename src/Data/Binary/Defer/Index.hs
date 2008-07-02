ae0fcaec2671a254cb64af59a8b509bc4fad62c1
Neil Mitchell <unknown>
2008-07-02 23:16:45 +0000

    Add IndexMutable to Data.Binary.Defer.Index

--- src/Data/Binary/Defer/Index.hs
+++ src/Data/Binary/Defer/Index.hs
@@ -2,33 +2,53 @@
 module Data.Binary.Defer.Index(
     Id,
     Index, newIndex,
-    Lookup, newLookup, lookupKey, lookupIndex
+    Lookup, newLookup, lookupKey, lookupIndex,
+    IndexMutable, newIndexMutable, getIndex, indexFreeze
     ) where
 
+import General.Util
 import Data.Binary.Defer
 import Data.Binary.Defer.Array
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.List
 
 type Id = Int
 
+
+---------------------------------------------------------------------
+-- INDEX
+
 newtype Index a = Index (Array a)
+
 
 -- | Items will obtain the Id's 0..length-1
 newIndex :: [a] -> Index a
 newIndex = Index . array
 
 
+instance BinaryDefer a => BinaryDefer (Index a) where
+    put (Index x) = put x
+    get = get1 Index
+
+instance Show a => Show (Index a) where
+    show (Index xs) = unlines $ map show $ elems xs
+
+instance Functor Index where
+    fmap f (Index x) = Index $ fmap f x
+
+
+---------------------------------------------------------------------
+-- LOOKUP
+
 newtype Lookup a = Lookup {lookupKey :: Id}
 
 newLookup :: Id -> Lookup a
-newLookup i = Lookup i
+newLookup = Lookup
 
 lookupIndex :: Lookup a -> Index a -> a
 lookupIndex (Lookup i) (Index xs) = xs ! i
 
-
-instance BinaryDefer a => BinaryDefer (Index a) where
-    put (Index x) = put x
-    get = get1 Index
 
 instance BinaryDefer (Lookup a) where
     put (Lookup key) = put key
@@ -38,11 +58,25 @@ instance BinaryDefer (Lookup a) where
     getFixed = getFixed1 Lookup
 
 
-instance Show a => Show (Index a) where
-    show (Index xs) = unlines $ map show $ elems xs
-
 instance Show (Lookup a) where
     show (Lookup key) = "#" ++ show key
 
-instance Functor Index where
-    fmap f (Index x) = Index $ fmap f x
+
+---------------------------------------------------------------------
+-- INDEXMUTABLE
+
+newtype IndexMutable a = IndexMutable (Map.Map a Id)
+
+
+newIndexMutable :: IndexMutable a
+newIndexMutable = IndexMutable Map.empty
+
+
+getIndex :: Ord a => a -> IndexMutable a -> (IndexMutable a, Lookup a)
+getIndex x (IndexMutable mp) = (IndexMutable mp2, Lookup $ fromMaybe n res)
+    where (res,mp2) = Map.insertLookupWithKey (\_ _ a -> a) x n mp
+          n = Map.size mp
+
+
+indexFreeze :: IndexMutable a -> Index a
+indexFreeze (IndexMutable mp) = newIndex $ map fst $ sortBy (compare `on` snd) $ Map.toList mp
