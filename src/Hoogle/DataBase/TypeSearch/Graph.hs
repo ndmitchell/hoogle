0a50457dd298cad9d06d4d245b190210b934c847
Neil Mitchell <unknown>
2008-07-04 16:00:05 +0000

    Start to flesh out the Graph search module

--- src/Hoogle/DataBase/TypeSearch/Graph.hs
+++ src/Hoogle/DataBase/TypeSearch/Graph.hs
@@ -5,7 +5,11 @@
     nodes and edges have already been paid for.
 -}
 
-module Hoogle.DataBase.TypeSearch.Graph where
+module Hoogle.DataBase.TypeSearch.Graph(
+    Graph, newGraph,
+    GraphResult(..), ArgPos, Binding,
+    GraphSearch, graphSearch, graphFound, graphFollow, graphCost, graphNext
+    ) where
 
 import Hoogle.DataBase.TypeSearch.Cost
 import Hoogle.DataBase.TypeSearch.Score
@@ -14,17 +18,21 @@ import Hoogle.DataBase.Aliases
 import Hoogle.DataBase.Item
 import Hoogle.TypeSig.All
 import Data.Binary.Defer.Index
+import qualified Data.Map as Map
+import Control.Monad.State
+import General.Code
 
 
 type ArgPos = Int
 type Binding = [(String,String)]
 
 
-data Graph = Graph (Index Node)
+data Graph = Graph (Map.Map TypeSig (Lookup Node)) (Index Node)
 
--- AnswerArg.TypeScore is invalid within a node
 data Node = Node [GraphResult] [(Lookup Node, Lookup Cost)]
 
+-- GraphResult.TypeScore is invalid within a node
+-- is not saved, is loaded as blank
 data GraphResult = GraphResult
     {graphResultEntry :: Lookup Entry
     ,graphResultPos :: ArgPos
@@ -36,8 +44,48 @@ data GraphResult = GraphResult
 ---------------------------------------------------------------------
 -- GRAPH CONSTRUCTION
 
+{-
+Data:
+
+1) Map TypeSig (Lookup Node, Node)
+
+The graph as it currently stands, with each node indexed by
+a TypeSig
+
+2) Set TypeSig
+
+Those nodes in the graph which have not yet been explored
+-}
+
+data S = S
+    {costs :: IndexMutable Cost
+    ,graph :: Map.Map TypeSig (Lookup Node, Node)
+    }
+
+
 newGraph :: Aliases -> Instances -> [(Lookup Entry, ArgPos, TypeSig)] -> IndexMutable Cost -> (IndexMutable Cost, Graph)
-newGraph as is = undefined
+newGraph as is xs cost = (costs sN, f (graph sN))
+    where
+        sN = execState (initialGraph xs >> populateGraph >> reverseLinks) s0
+        s0 = S cost Map.empty
+
+        f mp = Graph (Map.map fst mp)
+            (newIndex $ map snd $ sortFst $ Map.elems mp)
+
+
+-- add links between each step
+populateGraph :: State S ()
+populateGraph = undefined
+
+
+-- create the initial graph
+initialGraph :: [(Lookup Entry, ArgPos, TypeSig)] -> State S ()
+initialGraph = undefined
+
+
+-- add reverse links where you can, i.e. aliases
+reverseLinks :: State S ()
+reverseLinks = undefined
 
 
 ---------------------------------------------------------------------
