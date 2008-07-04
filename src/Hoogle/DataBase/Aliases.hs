6c834f833c18d0b6d5f59f55e3f38e81baf495b1
Neil Mitchell <unknown>
2008-07-04 13:45:05 +0000

    Tweak some of Aliases

--- src/Hoogle/DataBase/Aliases.hs
+++ src/Hoogle/DataBase/Aliases.hs
@@ -6,35 +6,39 @@ module Hoogle.DataBase.Aliases(
 
 import Hoogle.TextBase.All
 import Hoogle.TypeSig.All
-import qualified Data.Map as Map
+import qualified Data.Binary.Defer.Map as Map
 import Data.Binary.Defer
 
 newtype Aliases = Aliases (Map.Map String Alias)
                   deriving Show
 
 instance BinaryDefer Aliases where
-    put = undefined
-    get = undefined
+    put (Aliases a) = put a
+    get = get1 Aliases
 
 
 data Alias = Alias
     {name :: String -- the name of the Alias
-    ,args :: [String] -- the arguments to the type
-    ,free :: [String] -- free variables on the RHS but not LHS
-    ,rhs :: TypeSig -- the resulting type
-    ,forward :: Bool -- is it type, or the reverse
+    ,args :: [String] -- the free variables
+    ,rhs :: Type -- the resulting type
     } deriving Show
+
+instance BinaryDefer Alias where
+    put (Alias a b c) = put3 a b c
+    get = get3 Alias
 
 
 createAliases :: [TextItem] -> Aliases
-createAliases _ = Aliases Map.empty
-
+createAliases ti = Aliases $ Map.fromList
+    [ (name, Alias name [v | TVar v <- args] rhs)
+    | ItemAlias (TypeSig _ lhs) (TypeSig _ rhs) <- ti
+    , let (TLit name, args) = fromTApp lhs]
 
 
 -- these are all the one step alias followings
 -- [String] is free variables in and out
 -- does a deep search for aliases
-followAlias :: TypeSig -> [String] -> [([String],TypeSig)]
-followAlias _ _ = []
+followAlias :: TypeSig -> [TypeSig]
+followAlias _ = []
 
 
