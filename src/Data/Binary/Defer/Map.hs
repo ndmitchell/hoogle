d4d365746b5b0906a85915a8cda00b780625d38d
Neil Mitchell <unknown>
2008-07-05 18:01:16 +0000

    Change the Data.Binary.Defer.Map type, defer different things

--- src/Data/Binary/Defer/Map.hs
+++ src/Data/Binary/Defer/Map.hs
@@ -1,14 +1,21 @@
+{-|
+    Provides a BinaryDefer instance for Data.Map
+
+    Defers the entire thing, and every element, but not any keys
+    which should give just about the right balance of laziness
+-}
 
 module Data.Binary.Defer.Map(
     module Data.Map
     ) where
 
 import Control.Monad
+import Control.Arrow
 import Data.Map
 import Data.Binary.Defer
-import qualified Data.Binary.Defer.Vector as Vector
+import Data.Binary.Defer.Vector as Vector
 
 
-instance (Ord k, BinaryDefer k, BinaryDefer v) => BinaryDefer (Map k v) where
-    put = put . Vector.fromList . toAscList
-    get = liftM (fromAscList . Vector.toList) get
+instance (Ord k, BinaryDefer k, BinaryDefer v) => BinaryDefer (Data.Map.Map k v) where
+    put = putDefer . put . Vector.fromList . Prelude.map (id *** Defer) . Data.Map.toAscList
+    get = getDefer $ liftM (Data.Map.fromAscList . Prelude.map (id *** fromDefer) . Vector.toList) get
