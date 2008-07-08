a2ea7cfb8b47bfb95fe1176056475147fa38a79a
Neil Mitchell <unknown>
2008-07-08 17:44:08 +0000

    Add a TypeMap

---
+++ src/Data/TypeMap.hs
@@ -0,0 +1,29 @@
+
+module Data.TypeMap where
+
+import Control.Monad
+import Data.Dynamic
+import Data.Maybe
+import qualified Data.Map as Map
+
+newtype TypeMap = TypeMap (Map.Map TypeRep Dynamic)
+
+
+empty :: TypeMap
+empty = TypeMap Map.empty
+
+
+lookup :: Typeable a => TypeMap -> Maybe a
+lookup (TypeMap mp) = res
+    where res = liftM (fromJust . fromDynamic) $ Map.lookup (typeOf $ fromJust res) mp
+
+
+insert :: Typeable a => a -> TypeMap -> TypeMap
+insert a (TypeMap mp) = TypeMap $ Map.insert (typeOf a) (toDyn a) mp
+
+
+instance Ord TypeRep where
+    compare a b = compare (splitTyConApp a) (splitTyConApp b)
+
+instance Ord TyCon where
+    compare a b = compare (tyConString a) (tyConString b)
