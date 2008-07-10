e3cfefdb537c5c41dc6110b07fe75af277ae253f
Neil Mitchell <unknown>
2008-07-10 18:10:46 +0000

    Add options for generating profile info to the make.bat script

--- src/make.bat
+++ src/make.bat
@@ -1,4 +1,6 @@
-mkdir obj
-ghc --make -odir obj -hidir obj Main.hs -o hoogle.exe
+if not exist obj mkdir obj
+if not exist obj\norm mkdir obj\norm
+if not exist obj\prof mkdir obj\prof
 
-
+if "%1" == "prof" ghc -prof -auto-all --make -odir obj\prof -hidir obj\prof Main.hs -o hooglep.exe
+if "%1" == "" ghc --make -odir obj\norm -hidir obj\norm Main.hs -o hoogle.exe
