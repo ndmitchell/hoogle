e3a0a56b98c22bdfa3a7904539561aa7bb78b76b
Neil Mitchell <unknown>
2008-07-10 17:12:57 +0000

    Permit a -> b +foo, and don't treat it as a module signature

--- src/Test/Parse_Query.hs
+++ src/Test/Parse_Query.hs
@@ -17,6 +17,7 @@ parse_Query = do
     "(a b)" === defaultQuery{typeSig = Just (TypeSig [] (TApp (TVar "a") [TVar "b"]))}
     "map :: a -> b" === defaultQuery{names = ["map"], typeSig = Just (TypeSig [] (TFun [TVar "a",TVar "b"]))}
     "+Data.Map map" === defaultQuery{scope = [PlusModule ["Data","Map"]], names = ["map"]}
+    "a -> b +foo" === defaultQuery{scope = [PlusPackage "foo"], typeSig = Just (TypeSig [] (TFun [TVar "a",TVar "b"]))}
     "Data.Map.map" === defaultQuery{scope = [PlusModule ["Data","Map"]], names = ["map"]}
     "[a]" === defaultQuery{typeSig = Just (TypeSig [] (TApp (TLit "[]") [TVar "a"]))}
     "++" === defaultQuery{names = ["++"]}
