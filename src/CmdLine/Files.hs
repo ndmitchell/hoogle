82fe2ea7b65cdeb91610190ab42d2aff61b6c05e
Neil Mitchell <unknown>
2008-07-10 14:13:27 +0000

    Change the database location code in response to the better flags handling of files

--- src/CmdLine/Files.hs
+++ src/CmdLine/Files.hs
@@ -2,6 +2,7 @@
 module CmdLine.Files(getDataBaseFiles) where
 
 import CmdLine.Flag
+import General.Glob
 import General.Code
 import Hoogle.Query.All
 
@@ -10,38 +11,15 @@ import Hoogle.Query.All
 -- otherwise use the CmdFlag and any +package query flags
 getDataBaseFiles :: [CmdFlag] -> Query -> IO [FilePath]
 getDataBaseFiles flags q = do
-        (files,dirs) <- dataFileDir flags
-        let pkg1 = [x | PlusPackage x <- scope q]
-            pkg2 = if null pkg1 && null files then ["default"] else pkg1
-        files2 <- mapM (resolveName dirs) pkg2
-        return $ nub $ files++files2
-
-
--- return the (files,dirs), error message if one does not exist
-dataFileDir :: [CmdFlag] -> IO ([FilePath],[FilePath])
-dataFileDir flags = f [x | DataFile x <- flags]
+    let dataFil = [x | DataFile x <- flags]
+        plusPkg = [x | PlusPackage x <- scope q]
+    if null dataFil && null plusPkg then
+        liftM (:[]) $ resolve "default"
+     else do
+        rs <- mapM resolve plusPkg
+        return $ dataFil ++ rs
     where
-        f [] = return ([],[])
-        f (x:xs) = do
-            ft <- fileType x
-            (x,ft) <- if (ft == NotFound && not (hasExtension x))
-                      then do x <- return $ x <.> "hoo"
-                              liftM ((,) x) (fileType x)
-                      else return (x,ft)
-
-            when (ft == NotFound) $
-                exitMessage ["File given with the --data flag does not exist"
-                            ,"    " ++ x]
-            (fs,ds) <- f xs
-            return $ if ft == File then (x:fs,ds) else (fs,x:ds)
-
-
--- change PlusPackage flags from names to files
-resolveName :: [FilePath] -> String -> IO FilePath
-resolveName dirs x = f (dirs ++ ["."])
-    where
-        f [] = exitMessage ["DataBase not found","    " ++ x] >> return ""
-        f (d:ds) = do
-            let s = d </> x <.> "hoo"
-            b <- doesFileExist s
-            if b then return s else f ds
+        inc = [x | Include x <- flags]
+        resolve x = do
+            [x] <- globFile inc ["hoo"] x
+            return x
