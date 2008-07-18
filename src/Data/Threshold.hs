ed6406d07aa8333af2cceff948c1072c9a941c58
Neil Mitchell <unknown>
2008-07-18 17:51:32 +0000

    Add Dta.Threshold

---
+++ src/Data/Threshold.hs
@@ -0,0 +1,16 @@
+
+module Data.Threshold where
+
+import qualified Data.Heap as Heap
+
+data Threshold k v = Result k v | Threshold k
+
+
+threshold :: Ord k => [Threshold k v] -> [v]
+threshold = f Heap.empty
+    where
+        f h [] = Heap.elems h
+        f h (Threshold k:xs) = as ++ f h2 xs
+            where (as,h2) = Heap.popUntil k h
+        f h (Result k v:xs) = f (Heap.push k v h) xs
+
