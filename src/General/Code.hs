aff5f0ec0caa11ff93da5e9a6c573e4f60c3215e
Neil Mitchell <unknown>
2008-08-02 10:06:53 +0000

    Add assert to General.Code

--- src/General/Code.hs
+++ src/General/Code.hs
@@ -3,7 +3,7 @@ module General.Code(
     module General.Util, module Safe,
     module Control.Arrow, module Control.Monad,
     module Data.Char, module Data.Maybe, module Data.List,
-    module Debug.Trace,
+    module Debug.Trace, module Control.Exception,
     module System.Cmd, module System.Directory, module System.Environment,
     module System.Exit, module System.FilePath, module System.IO
     ) where
@@ -17,6 +17,7 @@ import Data.Char
 import Data.Maybe
 import Data.List
 import Debug.Trace(trace)
+import Control.Exception(assert)
 import System.Cmd
 import System.Directory
 import System.Environment
