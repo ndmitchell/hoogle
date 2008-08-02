fce63f090b5e6988aee04875bec79e9994b73582
Neil Mitchell <unknown>
2008-08-02 12:41:55 +0000

    Rewrite addResultAll to properly deal with missing arguments

--- src/Hoogle/DataBase/TypeSearch/Result.hs
+++ src/Hoogle/DataBase/TypeSearch/Result.hs
@@ -12,6 +12,7 @@ import Hoogle.TypeSig.All
 import Hoogle.Item.All
 import General.Code
 import Data.Typeable
+import qualified Data.IntSet as IntSet
 
 
 type ArgPos = Int
@@ -67,15 +68,27 @@ newResultAll arityQuery e
 
 addResultAll :: Instances -> TypeContext -> (Maybe ArgPos, ResultArg) -> ResultAll -> (ResultAll, [Result])
 addResultAll is cquery (pos,res) (ResultAll i e info) =
-        (ResultAll i e info2, if any null info2 then [] else catMaybes ans)
+        (ResultAll i e info2
+        ,mapMaybe (\(r:rs) -> newGraphsResults is cquery i e rs r) path)
     where
         ind = maybe 0 (+1) pos
         info2 = zipWith (\i x -> [res|i==ind] ++ x) [0..] info
 
-        ans = [ newGraphsResults is cquery i e rs r
-              | (r:rs) <- sequence $ info2 !!+ (ind,[res])
-              , disjoint $ map resultArgPos rs]
+        -- path returns a path through the ResultArg's
+        -- must skip badarg items
+        -- must take one element from 0
+        -- must use res from ind
+        path :: [[ResultArg]]
+        path = f i IntSet.empty $ zip [0..] info
 
+        f bad set [] = [[] | bad == 0]
+        f bad set ((i,x):xs)
+            | i == ind = map (res:) $ f bad set xs
+            | i == 0 = [r:rs | r <- x, rs <- f bad set xs]
+            | otherwise =
+                (if bad > 0 then f (bad-1) set xs else []) ++
+                [r:rs | r <- x, let rp = resultArgPos r, not $ rp `IntSet.member` set
+                      , rs <- f bad (IntSet.insert rp set) xs]
 
 
 newGraphsResults :: Instances -> TypeContext -> Int -> EntryInfo -> [ResultArg] -> ResultArg -> Maybe Result
