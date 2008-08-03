3bc3f113ae201ae3702fc799fc812d7c03863e63
Neil Mitchell <unknown>
2008-08-03 13:54:55 +0000

    Make the make opt do the right thing

--- src/make.bat
+++ src/make.bat
@@ -4,5 +4,5 @@ if not exist obj\prof mkdir obj\prof
 if not exist obj\opt  mkdir obj\opt
 
 if "%1" == "prof" ghc -prof -auto-all --make -odir obj\prof -hidir obj\prof Main.hs -o hooglep.exe
-if "%1" == "opt" ghc -O -prof -auto-all --make -odir obj\prof -hidir obj\prof Main.hs -o hoogleo.exe
+if "%1" == "opt" ghc -O --make -odir obj\opt -hidir obj\opt Main.hs -o hoogleo.exe
 if "%1" == "" ghc --make -odir obj\norm -hidir obj\norm Main.hs -o hoogle.exe
