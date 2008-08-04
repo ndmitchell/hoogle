0e9d1001e15478b72774484369c2d44526913d9c
Neil Mitchell <unknown>
2008-08-04 15:20:39 +0000

    Add version to the Paths_hoogle file

--- src/Paths_hoogle.hs
+++ src/Paths_hoogle.hs
@@ -1,5 +1,9 @@
+module Paths_hoogle where
 
-module Paths_hoogle (getDataFileName) where
+import Data.Version(Version(..))
+
+version :: Version
+version = Version {versionBranch = [4,0], versionTags = ["manual"]}
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName = return
