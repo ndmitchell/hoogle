2c64131c6c605f240a45ed2645ce28baa811a274
Neil Mitchell <unknown>
2008-08-05 13:34:36 +0000

    Change the javascript from DOS to Unix format

--- src/res/hoogle.js
+++ src/res/hoogle.js
@@ -7,18 +7,18 @@ function on_load()
 // add hoogle as a quicksearch tag, available on Firefox/Mozilla
 function addHoogle()
 {
-	addEngine('hoogle','png','Programming','4691');
+    addEngine('hoogle','png','Programming','4691');
 }
 
 function addEngine(name,ext,cat,pid)
 {
-	if ((typeof window.sidebar == "object") && (typeof window.sidebar.addSearchEngine == "function"))
-	{
-		window.sidebar.addSearchEngine(
-			'http://mycroft.mozdev.org/install.php/' + pid + "/" + name + ".src",
-			'http://mycroft.mozdev.org/install.php/' + pid + "/" + name + "."+ ext, name, cat);
-	} else {
-		alert("You will need a browser which supports Sherlock to install this plugin.");
-	}
+    if ((typeof window.sidebar == "object") && (typeof window.sidebar.addSearchEngine == "function"))
+    {
+        window.sidebar.addSearchEngine(
+            'http://mycroft.mozdev.org/install.php/' + pid + "/" + name + ".src",
+            'http://mycroft.mozdev.org/install.php/' + pid + "/" + name + "."+ ext, name, cat);
+    } else {
+        alert("You will need a browser which supports Sherlock to install this plugin.");
+    }
 }
 
