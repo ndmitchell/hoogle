28cc96e58b320017293b092e9398530979d3c01e
Neil Mitchell <unknown>
2008-08-11 17:19:07 +0000

    Add -i.. to the make for GHC, to account for Paths_hoogle

--- src/make.bat
+++ src/make.bat
@@ -3,6 +3,6 @@ if not exist obj\norm mkdir obj\norm
 if not exist obj\prof mkdir obj\prof
 if not exist obj\opt  mkdir obj\opt
 
-if "%1" == "prof" ghc -prof -auto-all --make -odir obj\prof -hidir obj\prof Main.hs -o hooglep.exe
-if "%1" == "opt" ghc -O --make -odir obj\opt -hidir obj\opt Main.hs -o hoogleo.exe
-if "%1" == "" ghc --make -odir obj\norm -hidir obj\norm Main.hs -o hoogle.exe
+if "%1" == "prof" ghc -prof -auto-all --make -odir obj\prof -hidir obj\prof Main.hs -o hooglep.exe -i. -i..
+if "%1" == "opt" ghc -O --make -odir obj\opt -hidir obj\opt Main.hs -o hoogleo.exe -i. -i..
+if "%1" == "" ghc --make -odir obj\norm -hidir obj\norm Main.hs -o hoogle.exe -i. -i..
