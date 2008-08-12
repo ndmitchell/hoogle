74be3f5fcc0f992a8bd2ee454fbe8a5f4af209f0
Neil Mitchell <unknown>
2008-08-12 13:30:08 +0000

    Render suggestions in a nicer manner

--- src/Hoogle/Query/Suggest.hs
+++ src/Hoogle/Query/Suggest.hs
@@ -5,6 +5,7 @@ import Data.TagStr
 import General.Code
 import Hoogle.DataBase.All
 import Hoogle.Query.Type
+import Hoogle.Query.Render
 
 
 suggestQuery :: [DataBase] -> Query -> Maybe TagStr
@@ -17,7 +18,8 @@ suggestQuery db q | any f (names q) = Just $ Str "Can't think of anything more i
 suggestQuery db q | isJust $ typeSig q =
     case suggestion db (fromJust $ typeSig q) of
         Nothing -> Nothing
-        Just (Left s) -> Just $ Str s
-        Just (Right t) -> Just $ Str $ "Did you mean: " ++ show t
+        Just (Left s) -> Just $ TagBold $ Str s
+        Just (Right t) -> Just $ Tags [TagBold $ Str "Did you mean: ", TagHyperlink ("query:" ++ q2) $ Str q2]
+            where q2 = showTagText $ renderQuery q{typeSig = Just t}
 
 suggestQuery db q = Nothing
