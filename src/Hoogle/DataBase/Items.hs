440dc34f538d3ebf02dbb123b725a985b9512495
Neil Mitchell <unknown>
2008-08-13 22:00:34 +0000

    Implement mergeItems

--- src/Hoogle/DataBase/Items.hs
+++ src/Hoogle/DataBase/Items.hs
@@ -86,4 +86,20 @@ addTextItem linkPkg ti doc = case ti of
 
 
 mergeItems :: [Items] -> Items
-mergeItems = head
+mergeItems [x] = x
+mergeItems xs = Items
+        (newIndex $ concat $ reverse ps)
+        (newIndex $ concat $ reverse ms)
+        (newIndex $ sortOn entryScore $ concat $ reverse es)
+    where
+        (pi,ps,mi,ms,ei,es) = foldl' f (0,[],0,[],0,[]) xs
+
+        f (pi,ps,mi,ms,ei,es) (Items p m e) =
+                (pi+length p3,p3:ps, mi+length m3,m3:ms, ei+length e3,e3:es)
+            where
+                (p2,p3) = add pi p id
+                (m2,m3) = add mi m $ \x -> x{modulePackage = p2 !! linkKey (modulePackage x)}
+                (e2,e3) = add ei e $ \x -> x{entryModule = liftM (\x -> m2 !! linkKey x) $ entryModule x}
+
+                add i xs f = (zipWith newLink [i..] xs2, xs2)
+                    where xs2 = map (f . fromLink) $ indexLinks xs
