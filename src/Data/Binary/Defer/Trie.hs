e4a8e1328edf71cd8be005e64eb7b0044457b2e0
Neil Mitchell <unknown>
2008-08-13 16:05:04 +0000

    Add unionsWith to Data.Trie

--- src/Data/Binary/Defer/Trie.hs
+++ src/Data/Binary/Defer/Trie.hs
@@ -1,6 +1,6 @@
 
 module Data.Binary.Defer.Trie(
-    Trie, newTrie, lookupTrie
+    Trie, newTrie, lookupTrie, unionsWith
     ) where
 
 import Data.Binary.Defer
@@ -10,6 +10,7 @@ import General.Code
 
 
 -- Keep simple first, can move to an Array later if necessary
+-- the second list is always ordered
 data Trie a = Trie (Maybe a) [(Char,Trie a)]
               deriving Eq
 
@@ -30,6 +31,29 @@ newTrieOrdered xs = Trie
 lookupTrie :: String -> Trie a -> Maybe a
 lookupTrie [] (Trie a b) = a
 lookupTrie (x:xs) (Trie a b) = lookup x b >>= lookupTrie xs
+
+
+unionsWith :: (a -> a -> a) -> [Trie a] -> Trie a
+unionsWith f [] = newTrie []
+unionsWith f [x] = x
+unionsWith f (x:xs) = foldl (unionWith f) x xs
+
+
+unionWith :: (a -> a -> a) -> Trie a -> Trie a -> Trie a
+unionWith merge (Trie x xs) (Trie y ys) = Trie xy $ f xs ys
+    where
+        xy = case (x,y) of
+                  (Just x, Nothing) -> Just x
+                  (Nothing, Just y) -> Just y
+                  (Just x,  Just y) -> Just $ merge x y
+                  (Nothing,Nothing) -> Nothing
+
+        f ((x1,x2):xs) ((y1,y2):ys) = case compare x1 y1 of
+            EQ -> (x1,unionWith merge x2 y2) : f xs ys
+            LT -> (x1,x2) : f xs ((y1,y2):ys)
+            GT -> (y1,y2) : f ((x1,x2):xs) ys
+        f xs [] = xs
+        f [] ys = ys
 
 
 instance BinaryDefer a => BinaryDefer (Trie a) where
