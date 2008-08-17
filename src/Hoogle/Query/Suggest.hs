a9bc150d142008a15da04b05cee7a942a57775c5
Neil Mitchell <unknown>
2008-08-17 08:55:26 +0000

    Add more suggestions, Maybe a suggests :: Maybe a

--- src/Hoogle/Query/Suggest.hs
+++ src/Hoogle/Query/Suggest.hs
@@ -6,15 +6,27 @@ import General.Code
 import Hoogle.DataBase.All
 import Hoogle.Query.Type
 import Hoogle.Query.Render
+import Hoogle.TypeSig.All
 
 
 suggestQuery :: [DataBase] -> Query -> Maybe TagStr
+
+-- They searched for Google (pay homage)
 suggestQuery db q | "google" `elem` map (map toLower) (names q) =
     Just $ Tags [TagHyperlink "http://www.google.com/" (Str "Google"), Str " rocks!"]
 
+-- They searched for ?oogle (mock)
 suggestQuery db q | any f (names q) = Just $ Str "Can't think of anything more interesting to search for?"
     where f x = length x == 6 && "oogle" `isSuffixOf` x
 
+-- They search for "Maybe a", did they mean ":: Maybe a"
+suggestQuery db q@Query{typeSig=Nothing, names=n:ames} | all f (n:ames) = Just $ didYouMean q2
+    where q2 = q{names = [], typeSig = Just $ TypeSig [] $ TApp (g n) (map g ames)}
+          f (x:xs) = if null xs then isLower x else isUpper x
+          g xs@(x:_) = if isLower x then TVar xs else TLit xs
+
+
+-- See what the type signature suggests from the database
 suggestQuery db q | isJust $ typeSig q =
     case suggestion db (fromJust $ typeSig q) of
         Nothing -> Nothing
