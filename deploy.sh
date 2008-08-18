48b39128873eb9184395a4e398604b3da876f7fa
Neil Mitchell <unknown>
2008-08-18 11:36:16 +0000

    Fix the deploy script to work with GHC 6.6.1 at York

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# NOTE: Use GHC 6.6 as haskell.org can't cope with GHC 6.8 binaries
-#       timer_create: Operation not supported
+# NOTE: Use GHC 6.6.1 as haskell.org can't cope with GHC 6.8 binaries
+#       given error "timer_create: Operation not supported"
+#       And York's GHC 6.6 has a broken time library.
 
 mkdir deploy/res --parents
-runhaskell Setup configure --with-compiler=ghc-6.6 --with-hc-pkg=ghc-pkg-6.6
+runhaskell Setup configure --with-compiler=ghc-6.6.1 --with-hc-pkg=ghc-pkg-6.6.1 --user
 runhaskell Setup build
 cp dist/build/hoogle/hoogle deploy/index.cgi
 deploy/index.cgi /convert=hoogle.txt /output=deploy/res/default.hoo
