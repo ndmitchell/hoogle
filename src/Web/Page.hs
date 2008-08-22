552f8c44be31f529ac06dc650f7979bd94eacb70
Neil Mitchell <unknown>
2008-08-22 13:52:56 +0000

    Add code to make the front page validate

--- src/Web/Page.hs
+++ src/Web/Page.hs
@@ -5,7 +5,7 @@ import Web.Text
 
 header query =
     ["<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
-    ,"<html>"
+    ,"<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>"
     ,"  <head profile='http://a9.com/-/spec/opensearch/1.1/'>"
     ,"     <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1' />"
     ,"     <title>" ++ (query +? " - ") ++ "Hoogle</title>"
@@ -27,12 +27,14 @@ links =
     ]
 
 search query =
-    ["<form action='?' method='get' id='search'>"
-    ,"  <a id='logo' href='http://haskell.org/hoogle/'>" ++
-         "<img src='res/hoogle.png' alt='Hoogle' />" ++
-       "</a>"
-    ,"  <input name='q' id='q' type='text' value=\"" ++ query ++ "\" />"
-    ,"  <input id='submit' type='submit' value='Search' />"
+    ["<form action='?' method='get'>"
+    ,"  <div id='search'>"
+    ,"    <a id='logo' href='http://haskell.org/hoogle/'>" ++
+           "<img src='res/hoogle.png' alt='Hoogle' />" ++
+         "</a>"
+    ,"    <input name='q' id='q' type='text' value=\"" ++ query ++ "\" />"
+    ,"    <input id='submit' type='submit' value='Search' />"
+    ,"  </div>"
     ,"</form>"
     ]
 
