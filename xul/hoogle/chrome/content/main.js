4844f72718ed1636b849371eafd287e1899ad777
Neil Mitchell <unknown>
2008-08-23 09:31:51 +0000

    Add some code to the main window

---
+++ xul/hoogle/chrome/content/main.js
@@ -0,0 +1,17 @@
+
+function test()
+{
+    alert("hello");
+    /*
+    var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile); 
+    file.initWithPath("start"); 
+
+    alert("here3");
+    var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess); 
+    process.init(file); 
+
+    var argv = ["/min","/wait", "hoogle","/web","/debug"];
+    alert("here3");
+    process.run(true, argv, argv.length);
+    */
+}
