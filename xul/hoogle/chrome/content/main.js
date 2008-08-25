f7edcd7326f90d58e03fdbdfe1365c6a1331c33b
Neil Mitchell <unknown>
2008-08-25 19:26:39 +0000

    More on the Hoogle GUI version

--- xul/hoogle/chrome/content/main.js
+++ xul/hoogle/chrome/content/main.js
@@ -1,7 +1,8 @@
 
-function test()
+function on_load()
 {
-    alert("hello");
+    i = document.getElementById("iframe");
+    i.contentDocument.body.setAttribute("id","xul");
     /*
     var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile); 
     file.initWithPath("start"); 
