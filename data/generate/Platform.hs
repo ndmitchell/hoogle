318a140b2574bf8f6a10702ebe078f074ce2869e
Neil Mitchell <unknown>
2009-06-21 12:14:12 +0000

    Include hackage in the list of default packages

--- data/generate/Platform.hs
+++ data/generate/Platform.hs
@@ -9,7 +9,7 @@ libs = words $
     "array base bytestring containers directory filepath ghc-prim haskell98 " ++
     "hpc integer old-locale old-time packedstring pretty random syb template-haskell " ++
     "cgi fgl editline haskell-src html HUnit mtl network parallel parsec " ++
-    "QuickCheck regex-base regex-compat regex-posix stm time xhtml zlib HTTP"
+    "QuickCheck regex-base regex-compat regex-posix stm time xhtml zlib HTTP hackage"
 
 bad = words "ghc-prim haskell98 integer old-locale packedstring"
 
