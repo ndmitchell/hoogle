47285c40df308319d97d1d733c132e4c0da91906
Neil Mitchell <unknown>
2010-09-14 07:14:36 +0000

    Repoint the Paths module at the correct database location

--- src/Paths.hs
+++ src/Paths.hs
@@ -1,15 +1,10 @@
-{-|
-    I would much rather put this module hidden away in src
-    but then Cabal will always use it in preference to the
-    auto-generated one.
--}
 
 module Paths_hoogle where
 
 import Data.Version(Version(..))
 
 version :: Version
-version = Version {versionBranch = [4,0], versionTags = ["hugs"]}
+version = Version {versionBranch = [4,0], versionTags = ["dev"]}
 
 getDataDir :: IO FilePath
-getDataDir = return "../database"
+getDataDir = return "database"
