d82e810620e30c7265283ea4ca5681c3afd0e417
Neil Mitchell <unknown>
2010-10-25 19:31:46 +0000

    Delete dead code

--- src/Console/Action.hs
+++ src/Console/Action.hs
@@ -55,27 +55,3 @@ action q@Search{} | isBlankQuery $ fromRight $ queryParsed q =
 
 
 action q@Search{} = actionSearch q (fromRight $ queryParsed q)
-
-
----------------------------------------------------------------------
--- SPECIFIC ACTIONS
-
-{-
-actionConvert :: FilePath -> FilePath -> IO ()
-actionConvert infile outfile = do
-    let outfile = headDef (replaceExtension infile "hoo") [x | Output x <- queryFlags q]
-    putStrLn $ "Converting " ++ infile
-    deps <- getDataBaseFilesNoDefault (queryFlags q) (fromRight $ query q)
-    convert (Debug `elem` queryFlags q) deps infile outfile
-    putStrLn $ "Written " ++ outfile
-    
-    when (Dump{} `elemEnum` queryFlags q) $ do
-        putStrLn ""
-        actionDump q outfile
-    return outfile
-
-actionTest :: CmdQuery -> FilePath -> IO ()
-actionTest q infile = do
-    outfile <- actionConvert q{queryFlags = Debug : queryFlags q} infile
-    testFile infile outfile
--}
