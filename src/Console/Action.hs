2b54bcea676eb6876fa811dd795c68d10501b28b
Neil Mitchell <unknown>
2010-10-25 19:34:07 +0000

    Split rank in to a separate module

--- src/Console/Action.hs
+++ src/Console/Action.hs
@@ -4,6 +4,7 @@ module Console.Action(action) where
 import CmdLine.All
 import Console.Search
 import Console.Test
+import Console.Rank
 import General.Code
 import Data.Monoid
 import Test.All
@@ -17,14 +18,11 @@ action Search{queryText = text, queryParsed = Left (ParseError _ pos err)} =
                 ,replicate pos ' ' ++ "^"
                 ,err]
 
-
 action (Test files) = do
     test
     mapM_ testFile files
 
-
-action (Rank file) = error "to rank, using scoring function" -- rank file
-
+action (Rank file) = rank file
 
 action (Convert from to) = do
     to <- return $ if null to then replaceExtension from "hoo" else to
@@ -36,22 +34,18 @@ action (Convert from to) = do
     saveDatabase to db
     putStrLn $ "Written " ++ to
 
-
 action (Combine from to) = do
     putStrLn $ "Combining " ++ show (length from) ++ " databases"
     xs <- mapM loadDatabase from
     saveDatabase to $ mconcat xs
-
 
 action (Dump file sections) = do
     d <- loadDatabase file
     putStrLn $ "File: " ++ file
     putStr $ showDatabase d $ if null sections then Nothing else Just sections
 
-
 action q@Search{} | isBlankQuery $ fromRight $ queryParsed q =
     exitMessage ["No query entered"
                 ,"Try --help for command line options"]
-
 
 action q@Search{} = actionSearch q (fromRight $ queryParsed q)
---
+++ src/Console/Rank.hs
@@ -0,0 +1,14 @@
+
+module Console.Rank(rank) where
+
+import CmdLine.All
+import Console.Search
+import Console.Test
+import General.Code
+import Data.Monoid
+import Test.All
+import Hoogle
+
+
+rank :: FilePath -> IO ()
+rank = error "todo: rank"
