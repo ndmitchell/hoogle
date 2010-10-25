206dcc52e6f835af5ba8ec64e19d58cab18a9d5b
Neil Mitchell <unknown>
2010-10-25 19:37:22 +0000

    Delete the Combine/Convert operations, were no longer used

--- src/Hoogle/Operations/All.hs
+++ src/Hoogle/Operations/All.hs
@@ -1,10 +1,6 @@
 
 module Hoogle.Operations.All(
-    module Hoogle.Operations.Combine,
-    module Hoogle.Operations.Convert,
     module Hoogle.Operations.Rank
     ) where
 
-import Hoogle.Operations.Combine
-import Hoogle.Operations.Convert
 import Hoogle.Operations.Rank
