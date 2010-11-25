9dddf0543dfa739a370a30f18573ab50a7c6678c
Neil Mitchell <unknown>
2010-11-25 09:01:23 +0000

    Rework the SubstrSearch to save time when making binary bits, now twice as fastg

--- src/Hoogle/DataBase/SubstrSearch.hs
+++ src/Hoogle/DataBase/SubstrSearch.hs
@@ -6,7 +6,10 @@ module Hoogle.DataBase.SubstrSearch
     ) where
 
 import Data.Binary.Defer
-import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BS
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Binary as Bin
 import qualified Data.Binary.Get as Bin
 import qualified Data.Binary.Put as Bin
@@ -15,6 +18,13 @@ import Hoogle.Type.All
 import Hoogle.Score.All
 import Data.Function
 
+
+-- idea for speed improvement
+-- store as one long bytestring with \0 between the words, then do findSubstrings to find the indexes
+-- store the lengths in a separate bytestring then use index to step through them, retrieving the data as Word8 via foldl
+-- store the links in another bytestring with the lengths, but only unpack them when they are needed
+-- can even make length==0 code for it's the same string as before, to compress it and reduce searching
+-- was previously ~ 0.047 seconds
 
 {-
 Description:
@@ -30,26 +40,60 @@ Data is stored flattened. For default we expect ~200Kb of disk usage.
 -}
 
 -- keys are sorted after being made lower case
-newtype SubstrSearch a = SubstrSearch [(BS.ByteString,a)]
+data SubstrSearch a = SubstrSearch
+    {text :: BS.ByteString -- all the bytestrings, lowercase, sorted
+    ,lens :: BS.ByteString -- a list of lengths, 0 means the string was identical to the previous one
+    ,inds :: Int -> a -- a way of retrieving each index
+    }
 
 
 -- | Create a substring search index. Values are returned in order where possible.
 createSubstrSearch :: [(String,a)] -> SubstrSearch a
-createSubstrSearch xs = SubstrSearch $ map (first BS.pack) . sortBy (compare `on` fst) $ map (first $ map toLower) xs
+createSubstrSearch xs = SubstrSearch
+    (BSC.pack $ concat ts2)
+    (BS.pack $ map fromIntegral ls2)
+    (is !!)
+    where
+        (ts,is) = unzip $ sortBy (compare `on` fst) $ map (first $ map toLower) xs
+        (ts2,ls2) = f "" ts
 
+        f x (y:ys) | x == y = second (0:) $ f x ys
+                   | otherwise = first (y:) $ second (length y:) $ f y ys
+        f x [] = ([],[])
+
+
+data S a = S
+    {sCount :: !Int -- which one are we on
+    ,sFocus :: !BS.ByteString -- where we are in the string
+    ,sLast :: !(Maybe TextMatch) -- the last result
+    ,sPrefix :: ![(a,EntryView,Score)] -- the prefixes
+    ,sInfix :: ![(a,EntryView,Score)] -- the infixes
+    }
 
 searchSubstrSearch :: Eq a => SubstrSearch a -> String -> [(a, EntryView, Score)]
-searchSubstrSearch (SubstrSearch xs) y =
-        find MatchExact (ly ==) $
-        find MatchPrefix (ly `BS.isPrefixOf`) $
-        find MatchSubstr (ly `BS.isInfixOf`) []
+searchSubstrSearch x y = reverse (sPrefix sN) ++ reverse (sInfix sN)
     where
         view = FocusOn y
-        ly = BS.pack $ map toLower y
+        ly = BSC.pack $ map toLower y
+        ny = length y
+        sN = BS.foldl f s0 $ lens x
+        s0 = S 0 (text x) Nothing [] []
 
-        find scr p rest = res ++ filter (flip notElem (map fst3 res) . fst3) rest
-            where res = [(x,view,textScore scr) | (s,x) <- xs, p s]
+        f s 0 = addCount $ case sLast s of
+            Nothing -> s
+            Just x -> addMatch x s
+        f s ii = addCount $ moveFocus i $ case () of
+            _ | ly `BS.isPrefixOf` str -> addMatchLast (if ny == i then MatchExact else MatchPrefix) s
+              | ly `BS.isInfixOf` str -> addMatchLast MatchSubstr s
+              | otherwise -> s{sLast=Nothing}
+            where str = BS.unsafeTake i $ sFocus s
+                  i = fromIntegral ii
 
+        addCount s = s{sCount=sCount s+1}
+        moveFocus i s = s{sFocus=BS.unsafeDrop i $ sFocus s}
+        addMatchLast t s = addMatch t s{sLast=Just t}
+        addMatch MatchSubstr s = s{sInfix =(inds x $ sCount s,view,textScore MatchSubstr):sInfix  s}
+        addMatch t s = s{sPrefix=(inds x $ sCount s,view,textScore t):sPrefix s}
 
 
 completionsSubstrSearch :: SubstrSearch a -> String -> [String]
@@ -59,30 +103,34 @@ completionsSubstrSearch _ _ = []
 instance Show a => Show (SubstrSearch a) where
     show x = "SubstrSearch"
 
-instance (Bin.Binary a, BinaryDeferGet a) => BinaryDefer (SubstrSearch a) where
+instance (Bin.Binary a, BinaryDeferGet a, FixedBinary a) => BinaryDefer (SubstrSearch a) where
     put x = putDefer $ putLazyByteString $ Bin.runPut $ putBinary Bin.put x
 
     get = do
         g <- binaryDeferGet
         x <- getDefer getLazyByteString
-        return $ Bin.runGet (getBinary g) x
+        return $ Bin.runGet (getBinary (fixedSize $ tyUnGet g) g) x
+        where
+            tyUnGet :: Bin.Get a -> a
+            tyUnGet = undefined
 
 
 putBinary :: (a -> Bin.Put) -> SubstrSearch a -> Bin.Put
-putBinary p (SubstrSearch x) = do
-    Bin.putWord32host $ fromIntegral $ length x
-    forM_ x $ \(s,a) -> do
-        Bin.putWord8 $ fromIntegral $ BS.length s
-        Bin.putByteString s
-        p a
+putBinary p x = do
+    Bin.put $ text x
+    Bin.put $ lens x
+    Bin.put $ fromLBS $ Bin.runPut $ mapM_ (p . inds x) [0.. fromIntegral (BS.length $ lens x) - 1]
 
 
-getBinary :: Bin.Get a -> Bin.Get (SubstrSearch a)
-getBinary g = do
-    i <- fmap fromIntegral Bin.getWord32host
-    fmap SubstrSearch $ replicateM i $ do
-        n <- Bin.getWord8
-        s <- Bin.getByteString (fromIntegral n)
-        a <- g
-        return (s,a)
+getBinary :: Int -> Bin.Get a -> Bin.Get (SubstrSearch a)
+getBinary size g = do
+    text <- Bin.get
+    lens <- Bin.get
+    indsData <- Bin.get
+    let inds i = Bin.runGet g $ toLBS $ BS.take size $ BS.drop (i * size) indsData
+    return $ SubstrSearch text lens inds
+
+
+fromLBS = BS.concat . LBS.toChunks
+toLBS = LBS.fromChunks . return
 
