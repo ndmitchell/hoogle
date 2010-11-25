7c4b310cbcccce41ee4277b8075d3ff77bb33f44
Neil Mitchell <unknown>
2010-11-25 19:43:37 +0000

    Put a javascript cache on the client side in the browser to speed up performance

--- datadir/resources/hoogle.js
+++ datadir/resources/hoogle.js
@@ -6,15 +6,28 @@ $(function(){
     $("#hoogle").focus();
     if ($.getQueryString('ajax'))
     {
+        var c = cache(100);
+        var last = $("#hoogle").val();
+
         $("#hoogle").keyup(function(){
-            $.ajax({
-                url: '?',
-                data: {mode:'ajax', 'hoogle':$("#hoogle").val()},
-                dataType: 'html',
-                complete: function(e){
-                    $("#body").html(e.responseText);
-                }
-            });
+            var now = $("#hoogle").val();
+            if (now == last) return; else last = now;
+            var old = c.ask(now);
+            if (old != undefined)
+                $("#body").html(old);
+            else
+            {
+                $.ajax({
+                    url: '?',
+                    data: {mode:'ajax', hoogle:now},
+                    dataType: 'html',
+                    complete: function(s){return function(e){
+                        c.add(s,e.responseText);
+                        if ($("#hoogle").val() == s)
+                            $("#body").html(e.responseText);
+                    }}(now)
+                });
+            }
         });
     }
 });
@@ -31,6 +44,32 @@ function docs(i)
     return false;
 }
 
+
+/////////////////////////////////////////////////////////////////////
+// CACHE
+
+function cache(maxElems)
+{
+    // FIXME: Currently does not evict things
+    var contents = {}; // what we have in the cache
+
+    return {
+        add: function(key,val)
+        {
+            contents[key] = val;
+        },
+        
+        ask: function(key)
+        {
+            return contents[key];
+        }
+    };
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+// EXTERNAL jQUERY BITS
 
 // From http://stackoverflow.com/questions/901115/get-querystring-values-with-jquery/3867610#3867610
 ;(function ($) {
