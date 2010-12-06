8056cda4a8f733b2de53851e14fe287dd1308cf0
Neil Mitchell <unknown>
2010-12-06 18:12:55 +0000

    Collapse Vector and Map for BinaryDefer, were only ever used together

--- src/Data/Binary/Defer/Map.hs
+++ src/Data/Binary/Defer/Map.hs
@@ -4,18 +4,25 @@
     Defers the entire thing, and every element, but not any keys
     which should give just about the right balance of laziness
 -}
-
-module Data.Binary.Defer.Map(
-    module Data.Map
-    ) where
+module Data.Binary.Defer.Map(module Map) where
 
 import Control.Monad
 import Control.Arrow
-import Data.Map
+import Data.Map as Map -- FIXME: If this is qualified, then the export doesn't work - no idea why
 import Data.Binary.Defer
-import Data.Binary.Defer.Vector as Vector
 
 
-instance (Ord k, BinaryDefer k, BinaryDefer v) => BinaryDefer (Data.Map.Map k v) where
-    put = putDefer . put . Vector.fromList . Prelude.map (second Defer) . Data.Map.toAscList
-    get = getDefer $ liftM (Data.Map.fromAscList . Prelude.map (second fromDefer) . Vector.toList) get
+instance (Ord k, BinaryDefer k, BinaryDefer v) => BinaryDefer (Map.Map k v) where
+    put = putDefer . put . Vector . Prelude.map (second Defer) . Map.toAscList
+    get = getDefer $ fmap (Map.fromAscList . Prelude.map (second fromDefer) . fromVector) get
+
+newtype Vector a = Vector {fromVector :: [a]}
+
+instance BinaryDefer a => BinaryDefer (Vector a) where
+    put (Vector xs) = putDefer $ do
+        putInt (length xs)
+        mapM_ put xs
+
+    get = getDefer $ do
+        i <- getInt
+        liftM Vector $ replicateM i get
