0d1d10144d2788f2cfb484c8cb7677ee77e5446d
Neil Mitchell <unknown>
2010-12-22 22:43:10 +0000

    The third bug was fixed in Haddock 2.8

--- misc/haddock/Haddock.hs
+++ misc/haddock/Haddock.hs
@@ -12,7 +12,7 @@ class  BUG1 a  where
 data a :**: b = Bug2 -- ^ BUG 2: The :**: is prefix without brackets
 
 
-data Bug3 = Bug3 {-# UNPACK #-} !Int -- ^ BUG 3: This Int gets two !!'s before it
+data Bug3 = Bug3 {-# UNPACK #-} !Int -- ^ FIXED 3 (Haddock 2.8): This Int gets two !!'s before it
 
 
 -- | BUG 4: The instance below has [incoherent] on it
