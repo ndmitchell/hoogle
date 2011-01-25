52eacc94cbb75f13fb9c517179f76161dbebab0a
Neil Mitchell <unknown>
2011-01-25 18:52:31 +0000

    Fix potential pattern match failures with suggestQuery, which crashed on "to"

--- src/Hoogle/Query/Suggest.hs
+++ src/Hoogle/Query/Suggest.hs
@@ -20,9 +20,11 @@ suggestQuery db q | any f (names q) = Just $ Str "Can't think of anything more i
     where f x = length x == 6 && "oogle" `isSuffixOf` x
 
 -- They searched for "Int to Float", they meant "Int -> Float"
-suggestQuery db q@Query{typeSig=Nothing, names=names} | "to" `elem` names = Just $ didYouMean q2
-    where q2 = fixup db $ q{names = [] ,typeSig = Just $ TypeSig [] t2}
-          t2 = TFun $ map (toApp . map toLitVar) $ split "to" names
+suggestQuery db q@Query{typeSig=Nothing, names=names}
+    | length parts > 1 && all (not . null) parts = Just $ didYouMean q2
+    where parts = split "to" names
+          q2 = fixup db $ q{names = [] ,typeSig = Just $ TypeSig [] t2}
+          t2 = TFun $ map (toApp . map toLitVar) parts
 
 -- They search for "Maybe a", did they mean ":: Maybe a"
 suggestQuery db q@Query{typeSig=Nothing, names=names} | length names > 1 && all f names = Just $ didYouMean q2
