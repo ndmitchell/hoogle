17e6e91a378c6d2777120793cd77122385a24652
Neil Mitchell <unknown>
2011-02-05 18:44:03 +0000

    Rework the Serialise code to retain binary compatibility

--- src/Hoogle/DataBase/Serialise.hs
+++ src/Hoogle/DataBase/Serialise.hs
@@ -11,23 +11,31 @@ import Hoogle.DataBase.Type
 
 
 -- FIXME: Has become hard coded, go back to minor version lumps
-hooVersion :: (Word8,Word8,Word8,Word8)
-hooVersion = (4,0,0,5)
-
+hooVersion = [4,0,0,5]
 hooString = "HOOG"
 
+data Identity = Identity
 
-data DB = DB String (Word8,Word8,Word8,Word8) (Defer DataBase)
+instance BinaryDefer Identity where
+    put Identity = mapM_ putChr hooString >> mapM_ putByte hooVersion
+    get = do
+        cs <- replicateM 4 getChr
+        vr <- replicateM 4 getByte
+        when (cs /= hooString) $
+            error $ "Not a hoogle database"
 
-instance BinaryDefer DB where
-    put (DB x1 x2 x3) = put3 x1 x2 x3
-    get = get3 DB
+        let showVer = intercalate "." . map show
+        when (vr /= hooVersion) $
+            error $ "Wrong hoogle database version: " ++ showVer vr ++
+                    " found, expected " ++ showVer hooVersion
+        return Identity
+
 
 
 saveDataBase :: FilePath -> DataBase -> IO ()
 saveDataBase file db = do
     h <- openBinaryFile file WriteMode
-    runDeferPut h $ put $ DB hooString hooVersion $ Defer db
+    runDeferPut h $ put (Identity, db)
     hClose h
 
 
@@ -38,13 +46,5 @@ loadDataBase file = do
         error $ "Not a hoogle database: " ++ file
 
     h <- openBinaryFile file ReadMode
-    DB str ver db <- runDeferGet h get
-
-    when (str /= hooString) $
-        error $ "Not a hoogle database: " ++ file
-
-    when (ver /= hooVersion) $
-        error $ "Wrong hoogle database version: " ++ show ver ++
-                " found, expected " ++ show hooVersion
-
-    return $ fromDefer db
+    (Identity,db) <- runDeferGet h get
+    return db
