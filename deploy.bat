ebfb84d0875a518909af7613bcaa7d78e33916bc
Neil Mitchell <unknown>
2011-04-30 18:06:34 +0000

    Start work on new deploy scripts

---
+++ deploy.bat
@@ -0,0 +1,31 @@
+@echo off
+echo Running Hoogle deploy script
+if "%1"=="--quick" goto remote
+
+REM Check no patches need pushing
+if exist deploy.patch del deploy.patch
+darcs send -o deploy.path --quiet
+if exist deploy.patch del deploy.patch && echo You must push first && goto error
+
+REM Build Hoogle
+if not exist .hpc mkdir .hpc
+if not exist .hpc\opt mkdir .hpc\opt
+ghc --make -isrc -i. src/Paths.hs src/Main.hs -w -odir .hpc/opt -hidir .hpc/opt -o .hpc/opt/hoogle -threaded -O -package transformers
+
+REM Run the tests
+.hpc\opt\hoogle data
+if not %errorlevel%==0 goto error
+.hpc\opt\hoogle test --example
+if not %errorlevel%==0 goto error
+
+:remote
+echo Starting to deploy
+ssh ndm@haskell.org -m deploy.sh
+
+
+goto end
+
+:error
+echo Error!
+
+:end
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-# NOTE: Use GHC 6.6.1 as haskell.org can't cope with GHC 6.8 binaries
-#       given error "timer_create: Operation not supported"
-#       And York's GHC 6.6 has a broken time library.
-
-mkdir deploy/res --parents
-runhaskell Setup configure --with-compiler=ghc-6.6.1 --with-hc-pkg=ghc-pkg-6.6.1 --user --datadir=res
-runhaskell Setup build
-cp dist/build/hoogle/hoogle deploy/index.cgi
-deploy/index.cgi /convert=hoogle.txt /output=deploy/res/default.hoo
-cp src/res/* deploy/res
-scp -r deploy/* haskell.org:/haskell/hoogle/beta
+set -e
+set -u
+echo Deploying on server
+set +u
+source ~/.profile
+set -u
+cd hoogle
+darcs pull --all
+cabal configure --datadir=/srv/web/haskell.org/hoogle/ --datasubdir=datadir -O2
+cabal build
+nice dist/build/hoogle/hoogle data all --redownload --datadir=/srv/web/haskell.org/hoogle/datadir2
+cp datadir/resources /srv/web/haskell.org/hoogle/res2
+cp dist/build/hoogle/index2.cgi
