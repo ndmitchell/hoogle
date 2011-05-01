8d87c75c5c2d2136f8d0082724e69ab2deb6f82f
Neil Mitchell <unknown>
2011-05-01 17:34:36 +0000

    More updates to the deploy script, get the copying and deployment working

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd hoogle
 darcs pull --all
 cabal configure --datadir=/srv/web/haskell.org/hoogle/ --datasubdir=datadir -O2
 cabal build
-nice dist/build/hoogle/hoogle data default all ghc --redownload --datadir=/srv/web/haskell.org/hoogle/datadir2
-cp datadir/resources /srv/web/haskell.org/hoogle/res2
-cp dist/build/hoogle/index2.cgi
+nice dist/build/hoogle/hoogle data default all ghc --redownload --datadir=/srv/web/haskell.org/hoogle/datadir2/databases || true
+cp -R datadir/resources /srv/web/haskell.org/hoogle/res2
+cp dist/build/hoogle/hoogle /srv/web/haskell.org/hoogle/index2.cgi
