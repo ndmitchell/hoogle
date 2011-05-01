3fddaddc9a3d00b26086c18b7a0a48f4d704e7e0
Neil Mitchell <unknown>
2011-05-01 16:34:31 +0000

    Ensure that deploy generates all the necessary databases, including default and ghc

--- deploy.bat
+++ deploy.bat
@@ -13,7 +13,7 @@ if not exist .hpc\opt mkdir .hpc\opt
 ghc --make -isrc -i. src/Paths.hs src/Main.hs -w -odir .hpc/opt -hidir .hpc/opt -o .hpc/opt/hoogle -threaded -O -package transformers
 
 REM Run the tests
-.hpc\opt\hoogle data
+.hpc\opt\hoogle data --redownload
 if not %errorlevel%==0 goto error
 .hpc\opt\hoogle test --example
 if not %errorlevel%==0 goto error
--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd hoogle
 darcs pull --all
 cabal configure --datadir=/srv/web/haskell.org/hoogle/ --datasubdir=datadir -O2
 cabal build
-nice dist/build/hoogle/hoogle data all --redownload --datadir=/srv/web/haskell.org/hoogle/datadir2
+nice dist/build/hoogle/hoogle data default all ghc --redownload --datadir=/srv/web/haskell.org/hoogle/datadir2
 cp datadir/resources /srv/web/haskell.org/hoogle/res2
 cp dist/build/hoogle/index2.cgi
