e9c230b740aed6d72ca26ed8914fa78205c90d6b
Neil Mitchell <ndmitchell@gmail.com>
2013-11-21 09:33:03 +0000

    Update the test program to find the right hoogle

--- src/Test.hs
+++ src/Test.hs
@@ -1,8 +1,18 @@
 
 module Main(main) where
 
+import Control.Monad
 import System.Cmd
+import System.Directory
 import System.Exit
+import System.FilePath
+
 
 main :: IO ()
-main = exitWith =<< system "hoogle test"
+main = do
+    let files = ["./dist/build/hoogle/hoogle.exe","./dist/build/hoogle/hoogle"
+                ,"./hoogle.exe","./hoogle"
+                ,"../hoogle/hoogle.exe","../hoogle/hoogle"]
+    found <- filterM doesFileExist files
+    res <- system $ normalise (head (found ++ ["hoogle"])) ++ " test"
+    exitWith res
