d7c704d67d813578101782b8475b03eefab301dc
Neil Mitchell <ndmitchell@gmail.com>
2013-12-31 13:27:42 +0000

    Update the version and libdir

--- src/Paths.hs
+++ src/Paths.hs
@@ -4,8 +4,8 @@ module Paths_hoogle where
 import Data.Version(Version(..))
 
 version :: Version
-version = Version {versionBranch = [4,1], versionTags = ["dev"]}
+version = Version {versionBranch = [4,2], versionTags = ["dev"]}
 
 getDataDir, getLibDir :: IO FilePath
 getDataDir = return "datadir"
-getLibDir = return "C:\\Program Files\\Haskell\\hoogle-4.1\\ghc-6.12.3"
+getLibDir = return "C:\\Program Files\\Haskell\\hoogle-4.2\\ghc-7.6.3"
