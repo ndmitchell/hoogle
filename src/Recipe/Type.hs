20035054bf07280541ecf86f8c4c3065f985e280
Neil Mitchell <ndmitchell@gmail.com>
2013-12-31 14:01:42 +0000

    Recipe, cut down some utility in Type that were unused

--- src/Recipe/Type.hs
+++ src/Recipe/Type.hs
@@ -1,9 +1,8 @@
 
 module Recipe.Type(
-    CmdLine(..), Name, noDeps, safeEncoding,
-    keywords, platform, cabals, inputs, listing, version,
-    resetWarnings, putWarning, recapWarnings,
-    outStr, outStrLn
+    CmdLine(..), Name,
+    listing, version,
+    resetWarnings, putWarning, recapWarnings
     ) where
 
 import CmdLine.All
@@ -15,25 +14,6 @@ import General.System
 
 
 type Name = String
-
-
-noDeps :: [Name] -> IO ()
-noDeps [] = return ()
-noDeps xs = error "Internal error: package with no dependencies had dependencies"
-
-
--- | Lots of things go slightly wrong if you use characters > 127 in places, this just replaces them with ?
-safeEncoding :: String -> String
-safeEncoding = map (\x -> if x <= '\0' || x > '\127' then '?' else x)
-
-
----------------------------------------------------------------------
--- DOWNLOADED INFORMATION
-
-keywords = "download/keyword.txt"
-platform = "download/haskell-platform.cabal"
-cabals = "download/hackage-cabal"
-inputs = "download/hackage-hoogle"
 
 listing :: FilePath -> IO [Name]
 listing dir = do
@@ -57,21 +37,13 @@ warnings = unsafePerformIO $ newMVar []
 
 putWarning :: String -> IO ()
 putWarning x = do
-    outStrLn x
+    putStrLn x
     modifyMVar_ warnings $ return . (x:)
 
 recapWarnings :: IO ()
 recapWarnings = do
     xs <- readMVar warnings
-    mapM_ outStrLn $ reverse xs
+    mapM_ putStrLn $ reverse xs
 
 resetWarnings :: IO ()
 resetWarnings = modifyMVar_ warnings $ const $ return []
-
-
-outputLock :: MVar ()
-outputLock = unsafePerformIO $ newMVar ()
-
-outStr, outStrLn :: String -> IO ()
-outStr x = withMVar outputLock $ \_ -> do putStr x; hFlush stdout
-outStrLn x = outStr $ x ++ "\n"
