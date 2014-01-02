a1770565eb3762b58a723d27251d557dfce4ed91
Neil Mitchell <ndmitchell@gmail.com>
2014-01-02 19:02:48 +0000

    Recipe, rearrange the rules making use of alternatives

--- src/Recipe/All.hs
+++ src/Recipe/All.hs
@@ -47,89 +47,6 @@ rules Data{..} = do
 
     packages <- newCache $ \() -> fmap (Set.fromList . lines) $ readFile' "downloads/packages.txt"
 
-    phony "all.hoo" $ do
-        pkgs <- packages ()
-        need $ map (<.> "hoo") $ "default" : Set.toList pkgs
-
-    "keyword.txt" *> \out -> do
-        let src = "downloads/keyword.htm"
-        need [src]
-        contents <- liftIO $ readFileUtf8' src
-        liftIO $ writeFileUtf8 out $ translateKeywords contents
-
-    "default.txt" *> \out -> do
-        writeFileLines out ["@combine keyword","@combine package","@combine platform"]
-
-    "platform.txt" *> \out -> do
-        let src = "downloads/platform.cabal"
-        contents <- readFile' src
-        writeFileLines out ["@combine " ++ x | x <- platformPackages contents]
-
-    "package.txt" *> \out -> do
-        need ["downloads/cabal.untar"]
-        xs <- liftIO $ listing "downloads/cabal"
-        xs <- liftIO $ forM xs $ \name -> do
-            ver <- version "downloads/cabal" name
-            let file = "downloads/cabal" </> name </> ver </> name <.> "cabal"
-            src <- readCabal file
-            return $ case src of
-                Nothing -> []
-                Just src ->
-                    [""] ++ zipWith (++) ("-- | " : repeat "--   ") (cabalDescription src) ++
-                    ["--","-- Version " ++ ver, "@url package/" ++ name, "@entry package " ++ name]
-        liftIO $ writeFileUtf8 out $ unlines $ "@url http://hackage.haskell.org/" : "@package package" : concat xs
-
-    (\x -> "*.txt" ?== x && takeBaseName x `notElem` ["keyword","default","platform","package"]) ?> \out -> do
-        need ["downloads/hoogle.untar","downloads/cabal.untar"]
-        let name = takeBaseName out
-            base = name == "base"
-        vc <- liftIO $ version "downloads/cabal" name
-        vh <- liftIO $ if base then return vc else version "downloads/hoogle" name
-        let hoo = if base then "downloads/base.txt" else "downloads/hoogle" </> name </> vh </> "doc" </> "html" </> name <.> "txt"
-            cab = "downloads/cabal" </> name </> vc </> name <.> "cabal"
-        need [hoo, cab]
-        hoo <- liftIO $ readFileUtf8' hoo `E.catch` \(_ :: SomeException) -> readFile hoo
-        deps <- liftIO $ fmap (maybe [] cabalDepends) $ readCabal cab
-        let cleanDeps = deps \\ (name:avoid)
-        loc <- liftIO $ findLocal local name
-        liftIO $ writeFileUtf8 out $ unlines $
-            ["@depends " ++ a | a <- cleanDeps] ++ haddockHacks loc (lines hoo)
-
-    imported <- newCache $ \file -> do
-        need [file]
-        xs <- liftIO $ readFileUtf8' file
-        return [x | x <- lines xs, takeWhile (not . isSpace) x `elem` ["type","data","newtype","class","instance","@depends"]]
-    let listDeps = map (drop 9) . takeWhile ("@depends " `isPrefixOf`)
-
-    let genImported pkgs seen [] = return []
-        genImported pkgs seen (t:odo)
-            | t `Set.member` seen || not (t `Set.member` pkgs) = genImported pkgs seen odo
-            | otherwise = do
-                i <- imported $ t <.> "txt"
-                let deps = listDeps i
-                fmap (i++) $ genImported pkgs (Set.insert t seen) (deps++odo)
-
-    (\x -> "*.hoo" ?== x && x /= "all.hoo") ?> \out -> do
-        let src = out -<.> "txt"
-        need [src]
-        contents <- liftIO $ fmap lines $ readFileUtf8' src
-        if not (null contents) && "@combine " `isPrefixOf` head contents then do
-            let deps = [x <.> "hoo" | x <- contents, Just x <- [stripPrefix "@combine " x]]
-            need deps
-            dbs <- liftIO $ mapM loadDatabase deps
-            putNormal $ "Creating " ++ out ++ " from " ++ show (length deps) ++ " databases... "
-            liftIO $ performGC
-            liftIO $ saveDatabase out $ mconcat dbs
-         else do
-            pkgs <- packages ()
-            deps <- genImported pkgs (Set.singleton $ takeBaseName out) $ listDeps contents
-            let (err,db) = createDatabase Haskell [snd $ createDatabase Haskell [] $ unlines deps] $ unlines contents
-            unless (null err) $ putNormal $ "Skipped " ++ show (length err) ++ " warnings in " ++ out
-            putLoud $ unlines $ map show err
-            putNormal $ "Creating " ++ out ++ "... "
-            liftIO $ performGC
-            liftIO $ saveDatabase out db
-
     "//*.tar" *> \out -> do
         let src = out <.> "gz"
         need [src]
@@ -145,6 +62,91 @@ rules Data{..} = do
         let Just url = lookup (takeFileName out) urls
         -- liftIO $ copyFile ("C:/spacework/hoogle/cache" </> takeFileName out) out
         wget url out
+
+    alternatives $ do -- *.txt
+        "keyword.txt" *> \out -> do
+            let src = "downloads/keyword.htm"
+            need [src]
+            contents <- liftIO $ readFileUtf8' src
+            liftIO $ writeFileUtf8 out $ translateKeywords contents
+
+        "default.txt" *> \out -> do
+            writeFileLines out ["@combine keyword","@combine package","@combine platform"]
+
+        "platform.txt" *> \out -> do
+            let src = "downloads/platform.cabal"
+            contents <- readFile' src
+            writeFileLines out ["@combine " ++ x | x <- platformPackages contents]
+
+        "package.txt" *> \out -> do
+            need ["downloads/cabal.untar"]
+            xs <- liftIO $ listing "downloads/cabal"
+            xs <- liftIO $ forM xs $ \name -> do
+                ver <- version "downloads/cabal" name
+                let file = "downloads/cabal" </> name </> ver </> name <.> "cabal"
+                src <- readCabal file
+                return $ case src of
+                    Nothing -> []
+                    Just src ->
+                        [""] ++ zipWith (++) ("-- | " : repeat "--   ") (cabalDescription src) ++
+                        ["--","-- Version " ++ ver, "@url package/" ++ name, "@entry package " ++ name]
+            liftIO $ writeFileUtf8 out $ unlines $ "@url http://hackage.haskell.org/" : "@package package" : concat xs
+
+        "*.txt" *> \out -> do
+            need ["downloads/hoogle.untar","downloads/cabal.untar"]
+            let name = takeBaseName out
+                base = name == "base"
+            vc <- liftIO $ version "downloads/cabal" name
+            vh <- liftIO $ if base then return vc else version "downloads/hoogle" name
+            let hoo = if base then "downloads/base.txt" else "downloads/hoogle" </> name </> vh </> "doc" </> "html" </> name <.> "txt"
+                cab = "downloads/cabal" </> name </> vc </> name <.> "cabal"
+            need [hoo, cab]
+            hoo <- liftIO $ readFileUtf8' hoo `E.catch` \(_ :: SomeException) -> readFile hoo
+            deps <- liftIO $ fmap (maybe [] cabalDepends) $ readCabal cab
+            let cleanDeps = deps \\ (name:avoid)
+            loc <- liftIO $ findLocal local name
+            liftIO $ writeFileUtf8 out $ unlines $
+                ["@depends " ++ a | a <- cleanDeps] ++ haddockHacks loc (lines hoo)
+
+    alternatives $ do -- *.hoo
+        phony "all.hoo" $ do
+            pkgs <- packages ()
+            need $ map (<.> "hoo") $ "default" : Set.toList pkgs
+
+        imported <- newCache $ \file -> do
+            need [file]
+            xs <- liftIO $ readFileUtf8' file
+            return [x | x <- lines xs, takeWhile (not . isSpace) x `elem` ["type","data","newtype","class","instance","@depends"]]
+        let listDeps = map (drop 9) . takeWhile ("@depends " `isPrefixOf`)
+
+        let genImported pkgs seen [] = return []
+            genImported pkgs seen (t:odo)
+                | t `Set.member` seen || not (t `Set.member` pkgs) = genImported pkgs seen odo
+                | otherwise = do
+                    i <- imported $ t <.> "txt"
+                    let deps = listDeps i
+                    fmap (i++) $ genImported pkgs (Set.insert t seen) (deps++odo)
+
+        "*.hoo" *> \out -> do
+            let src = out -<.> "txt"
+            need [src]
+            contents <- liftIO $ fmap lines $ readFileUtf8' src
+            if not (null contents) && "@combine " `isPrefixOf` head contents then do
+                let deps = [x <.> "hoo" | x <- contents, Just x <- [stripPrefix "@combine " x]]
+                need deps
+                dbs <- liftIO $ mapM loadDatabase deps
+                putNormal $ "Creating " ++ out ++ " from " ++ show (length deps) ++ " databases... "
+                liftIO $ performGC
+                liftIO $ saveDatabase out $ mconcat dbs
+             else do
+                pkgs <- packages ()
+                deps <- genImported pkgs (Set.singleton $ takeBaseName out) $ listDeps contents
+                let (err,db) = createDatabase Haskell [snd $ createDatabase Haskell [] $ unlines deps] $ unlines contents
+                unless (null err) $ putNormal $ "Skipped " ++ show (length err) ++ " warnings in " ++ out
+                putLoud $ unlines $ map show err
+                putNormal $ "Creating " ++ out ++ "... "
+                liftIO $ performGC
+                liftIO $ saveDatabase out db
 
 
 urls :: [(FilePath, URL)]
