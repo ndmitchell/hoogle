54fde9832361c640f814b6e3d99e3371a73168cb
Neil Mitchell <ndmitchell@gmail.com>
2014-01-03 10:46:52 +0000

    Recipe, don't open the file if there are no warnings to write out

--- src/Recipe/All.hs
+++ src/Recipe/All.hs
@@ -176,7 +176,7 @@ withWarnings act = do
     count <- newMVar 0
     let file = ".warnings"
     writeFile file ""
-    act $ \xs -> modifyMVar_ count $ \i -> do
+    act $ \xs -> unless (null xs) $ modifyMVar_ count $ \i -> do
         appendFile file $ unlines xs
         return $! i + length xs
     i <- readMVar count
