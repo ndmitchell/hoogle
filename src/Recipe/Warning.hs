b37df8def3c17b256fc6f27b9edc06e3a460b46e
Neil Mitchell <ndmitchell@gmail.com>
2014-01-03 10:43:14 +0000

    Recipe, change the warnings to always end up in .warnings

--- src/Recipe/Warning.hs
+++ src/Recipe/Warning.hs
@@ -3,15 +3,14 @@ module Recipe.Warning(
     withWarnings
     ) where
 
-import General.System
 import Control.Concurrent
 
 
 withWarnings :: (([String] -> IO ()) -> IO ()) -> IO (Int, FilePath)
 withWarnings act = do
     count <- newMVar 0
-    (file, handle) <- openTempFile "" "hoogle_errors_.txt"
-    hClose handle
+    let file = ".warnings"
+    writeFile file ""
     act $ \xs -> modifyMVar_ count $ \i -> do
         appendFile file $ unlines xs
         return $! i + length xs
