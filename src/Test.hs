c7660190cc5ad59be8304c23f84918b717807029
Iustin Pop <iustin@debian.org>
2014-07-30 00:15:44 +0200

    Expand test suite to be able to test `--no-download`
    The test suite can only test success cases, so it needs a bit of adjustement
    and a new dependency on temporary in order to setup the environment for
    `--no-download` testing.
    
    I've left the existing tests to run in the default data directory, although
    this means it's not a hermetic test, I think.

--- src/Test.hs
+++ src/Test.hs
@@ -1,4 +1,3 @@
-
 module Main(main) where
 
 import Control.Monad
@@ -7,6 +6,7 @@ import System.Directory
 import System.Exit
 import System.FilePath
 import System.Environment
+import System.IO.Temp
 
 
 main :: IO ()
@@ -15,14 +15,21 @@ main = do
                 ,"./hoogle.exe","./hoogle"
                 ,"../hoogle/hoogle.exe","../hoogle/hoogle"]
     found <- filterM doesFileExist files
-    let hoogle args = do
+    let hoogle args want_success = do
             let cmd = normalise (head (found ++ ["hoogle"])) ++ " " ++ args
             res <- system cmd
-            when (res /= ExitSuccess) $ error $ "Command: " ++ cmd ++ "\nFailed with: " ++ show res
+            when (res /= ExitSuccess && want_success) $
+                 error $ "Command: " ++ cmd ++ "\nFailed with: " ++ show res
+            when (res == ExitSuccess && not want_success) $
+                 error $ "Command: " ++ cmd ++ "\nExpected failure but didn't fail."
 
     args <- getArgs
     if "--no-net" `elem` args then
-        hoogle "test"
+        hoogle "test" True
      else do
-        hoogle "data"
-        hoogle "test --example"
+        hoogle "data" True
+        hoogle "test --example" True
+
+    -- Check --no-download functionality in an empty data directory.
+    withSystemTempDirectory "hoogle-no-download.test." $ \tempdir -> do
+         hoogle ("data --no-download -d" ++ tempdir) False
