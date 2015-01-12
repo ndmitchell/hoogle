4069066e43bf0e88b511344a3db63d9ffbd9ef89
Neil Mitchell <ndmitchell@gmail.com>
2015-01-12 17:12:57 +0000

    Improve the name filter a bit

--- src/ParseCabal.hs
+++ src/ParseCabal.hs
@@ -10,5 +10,5 @@ parseCabal src = f ["license"] ++ f ["category"] ++ f ["author","maintainer"]
     where
         f name = nub [ "@" ++ head name ++ " " ++ intercalate ", " xs
                      | x <- lines src, let (a,b) = break (== ':') x, lower a `elem` name
-                     , let xs = filter (/= "") $ map g $ splitOn "," $ drop 1 b, not $ null xs]
-        g = unwords . filter ('@' `notElem`) . words
+                     , let xs = filter (/= "") $ map g $ split (`elem` ",&") $ drop 1 b, not $ null xs]
+        g = unwords . filter ('@' `notElem`) . words . takeWhile (`notElem` "<(")
