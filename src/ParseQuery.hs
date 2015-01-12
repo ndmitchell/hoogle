b3839e198b3b478632e0768d48b3cf4bbe0a4984
Neil Mitchell <ndmitchell@gmail.com>
2015-01-12 11:40:22 +0000

    Allow queries to be parsed as types

--- src/ParseQuery.hs
+++ src/ParseQuery.hs
@@ -2,12 +2,16 @@
 module ParseQuery(parseQuery) where
 
 import Data.List
+import Data.Tuple.Extra
+import Language.Haskell.Exts
 import Type
 
 parseQuery :: String -> Query
-parseQuery x = Query (map f cat) names Nothing
+parseQuery x | "::":xs <- names = Query cat [] (Just $ fromParseResult $ parseType $ unwords xs)
+             | otherwise = Query cat names Nothing
     where
-        (cat,names) = partition (':' `elem`) $ words x
+        (cat,names) = first (map f) $ partition (\x -> not (":" `isPrefixOf` x) && ':' `elem` x) $ words x
+
         f ('+':xs) = f xs
         f ('-':xs) = let QTag _ a b = f xs in QTag False a b
         f xs = let (a,_:b) = break (== ':') xs in QTag True a b
