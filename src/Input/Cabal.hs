68987834b0b01f41abe08c09f6136e8ebcbdf6f9
Neil Mitchell <ndmitchell@gmail.com>
2015-01-14 07:30:28 +0000

    Add a utility function for reading a tarball

--- src/Input/Cabal.hs
+++ src/Input/Cabal.hs
@@ -4,30 +4,26 @@ module Input.Cabal(parseCabal) where
 
 import Data.List.Extra
 import System.FilePath
-import Codec.Compression.GZip as GZip
-import Codec.Archive.Tar as Tar
+import Control.Applicative
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.ByteString.Lazy.Char8 as LBS
+import Util
 
 
 -- items are stored as:
 -- QuickCheck/2.7.5/QuickCheck.cabal
 -- QuickCheck/2.7.6/QuickCheck.cabal
 parseCabal :: [String] -> IO (Map.Map String [String])
-parseCabal want = do
-    src <- LBS.readFile "input/cabal.tar.gz"
-    return $ f Map.empty $ Tar.read $ GZip.decompress src
+parseCabal want = foldl f Map.empty <$> tarballReadFiles "input/cabal.tar.gz"
     where
         wanted = Set.fromList want
 
         -- rely on the fact the highest version is last, and lazy evaluation
         -- skips us from actually parsing the previous values
-        f mp (Next e rest) | pkg `Set.member` wanted, NormalFile bs _ <- entryContent e
-                           = f (Map.insert pkg (extractCabal $ LBS.unpack bs) mp) rest
-            where pkg = takeBaseName $ entryPath e
-        f mp (Next _ rest) = f mp rest
-        f mp _ = mp
+        f mp (name, body) | pkg `Set.member` wanted = Map.insert pkg (extractCabal $ LBS.unpack body) mp
+                          | otherwise = mp
+            where pkg = takeBaseName name
 
 extractCabal :: String -> [String]
 extractCabal src = f ["license"] ++ f ["category"] ++ f ["author","maintainer"]
--- src/Util.hs
+++ src/Util.hs
@@ -2,12 +2,17 @@
 module Util(
     fileSize,
     pretty,
-    fromName, fromTyVarBind
+    fromName, fromTyVarBind,
+    tarballReadFiles
     ) where
 
 import System.IO
 import Language.Haskell.Exts
 import Data.List.Extra
+import qualified Data.ByteString.Lazy as LBS
+import Control.Applicative
+import Codec.Compression.GZip as GZip
+import Codec.Archive.Tar as Tar
 
 
 fileSize :: FilePath -> IO Int
@@ -23,3 +28,12 @@ fromName (Symbol x) = x
 fromTyVarBind :: TyVarBind -> Name
 fromTyVarBind (KindedVar x _) = x
 fromTyVarBind (UnkindedVar x) = x
+
+
+tarballReadFiles :: FilePath -> IO [(FilePath, LBS.ByteString)]
+tarballReadFiles file = f . Tar.read . GZip.decompress <$> LBS.readFile file
+    where
+        f (Next e rest) | NormalFile body _ <- entryContent e = (entryPath e, body) : f rest
+        f (Next _ rest) = f rest
+        f Done = []
+        f (Fail e) = error $ "tarballReadFiles on " ++ file ++ ", " ++ show e
