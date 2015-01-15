49eb1b29c7ad6535b6194b1366f57e822f06e064
Neil Mitchell <ndmitchell@gmail.com>
2015-01-15 17:00:09 +0000

    Rewrite the Cabal module to make sure it frees the memory quickly

--- src/Input/Cabal.hs
+++ src/Input/Cabal.hs
@@ -5,6 +5,7 @@ module Input.Cabal(parseCabal) where
 import Data.List.Extra
 import System.FilePath
 import Control.Applicative
+import Control.DeepSeq
 import Data.Maybe
 import Data.Tuple.Extra
 import qualified Data.Map as Map
@@ -15,17 +16,21 @@ import Util
 -- items are stored as:
 -- QuickCheck/2.7.5/QuickCheck.cabal
 -- QuickCheck/2.7.6/QuickCheck.cabal
+-- rely on the fact the highest version is last (using lastValues)
 parseCabal :: (String -> Bool) -> IO (Map.Map String [String])
 parseCabal want = do
     rename <- map (both trim . second (drop 1) . break (== '=')) . lines <$> readFile "misc/tag-rename.txt"
-    foldl (f rename) Map.empty <$> tarballReadFiles "input/cabal.tar.gz"
+    foldl' (f rename) Map.empty . filter (want . fst) . lastValues . map (first takeBaseName) <$> tarballReadFiles "input/cabal.tar.gz"
     where
-        -- rely on the fact the highest version is last, and lazy evaluation
-        -- skips us from actually parsing the previous values
-        f rename mp (name, body)
-            | want pkg = Map.insert pkg (extractCabal rename $ LBS.unpack body) mp
-            | otherwise = mp
-            where pkg = takeBaseName name
+        f rename mp (pkg,body) = rnf pkg `seq` rnf res `seq` Map.insert pkg res mp
+            where res = extractCabal rename $ LBS.unpack body
+
+
+lastValues :: Eq a => [(a,b)] -> [(a,b)]
+lastValues ((a1,_):(a2,x):xs) | a1 == a2 = lastValues $ (a2,x):xs
+lastValues (x:xs) = x : lastValues xs
+lastValues [] = []
+
 
 extractCabal :: [(String, String)] -> String -> [String]
 extractCabal rename src = f ["license"] ++ f ["category"] ++ f ["author","maintainer"]
