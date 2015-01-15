340c5f84307348482356fde0ab4ccbb6e72dec86
Neil Mitchell <ndmitchell@gmail.com>
2015-01-15 10:40:07 +0000

    Add a travis test script

---
+++ travis.hs
@@ -0,0 +1,17 @@
+
+import System.Time.Extra
+import System.Process.Extra
+
+
+main :: IO ()
+main = do
+    let timed x = do
+        putStrLn $ "\n\nSearching for " ++ x
+        (time,_) <- duration $ system $ "hogle " ++ (if x == "" then "" else show x)
+        putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
+        putStrLn "\n\n"
+    timed ""
+    timed "map"
+    timed "map package:base"
+    timed "a -> b"
+    timed "ShakeOptions -> a"
