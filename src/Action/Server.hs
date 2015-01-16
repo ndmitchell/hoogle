1d93a3227595c2a7e084df2462e72c84236633ed
Neil Mitchell <ndmitchell@gmail.com>
2015-01-16 19:53:36 +0000

    Use renderQuery to display the query

--- src/Action/Server.hs
+++ src/Action/Server.hs
@@ -29,9 +29,9 @@ spawn :: Database -> IO ()
 spawn pkg = server 80 $ \Input{..} -> case inputURL of
     [] -> do
         let grab name = [x | (a,x) <- inputArgs, a == name, x /= ""]
-        results <- unsafeInterleaveIO $ search pkg $
-            parseQuery (unwords $ grab "hoogle") <> Query (map parseScope $ grab "scope") [] Nothing
-        let body = showResults (unwords $ grab "hoogle" ++ grab "scope") results
+        let q = parseQuery (unwords $ grab "hoogle") <> Query (map parseScope $ grab "scope") [] Nothing
+        results <- unsafeInterleaveIO $ search pkg q
+        let body = showResults q results
         index <- unsafeInterleaveIO $ readFile "html/index.html"
         welcome <- unsafeInterleaveIO $ readFile "html/welcome.html"
         tags <- unsafeInterleaveIO $ concatMap (\x -> "<option" ++ (if x `elem` grab "scope" then " selected=selected" else "") ++ ">" ++ x ++ "</option>") . listTags <$> readTags pkg
@@ -43,8 +43,8 @@ spawn pkg = server 80 $ \Input{..} -> case inputURL of
     xs -> return $ OutputFile $ joinPath $ "html" : xs
 
 
-showResults :: String -> [[String]] -> String
+showResults :: Query -> [[String]] -> String
 showResults query results = unlines $
-    ["<h1>" ++ escapeHTML query ++ "</h1>"] ++
+    ["<h1>" ++ renderQuery query ++ "</h1>"] ++
     ["<p>No results found</p>" | null results] ++
     ["<div class=ans><a href=\"" ++ b ++ "\">" ++ escapeHTML (snd $ word1 a) ++ "</a></div><div class=from>" ++ c ++ "</div><div class=\"doc newline shut\">" ++ replace "<p>" "" (replace "</p>" "<br/>" $ unlines ds) ++ "</div>" | a:b:c:ds <- results]
