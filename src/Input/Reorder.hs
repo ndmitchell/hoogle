ea8aefb476589247abe0c0c84828d745a4ca4a2a
Neil Mitchell <ndmitchell@gmail.com>
2015-01-17 13:24:26 +0000

    Reorder the modules within the base library

--- src/Input/Reorder.hs
+++ src/Input/Reorder.hs
@@ -5,25 +5,38 @@ module Input.Reorder(reorderItems) where
 import Input.Type
 import Data.List.Extra
 import Data.Char
+import Data.Maybe
 import qualified Data.Map as Map
 
 
 reorderItems :: [(a, Items)] -> IO [(a, Items)]
 reorderItems xs = do
-    order <- readOrdering
-    return $ concatMap snd $ sortOn (maybe 0 negate . flip Map.lookup order . fst) $ splitPackages xs
+    packageOrder <- packageOrder
+    let rebase ("base",xs) = ("base", concatMap snd $ sortOn (baseModuleOrder . fst) $ splitModules xs)
+        rebase x = x
+    return $ concatMap snd $ sortOn (packageOrder . fst) $ map rebase $ splitPackages xs
 
 
-splitPackages :: [(a, Items)] -> [(String, [(a, Items)])]
-splitPackages = repeatedly $ \(x:xs) ->
-    let (a,b) = break (isIPackage . snd) xs
-    in ((case snd x of IPackage x -> x; _ -> "", x:a), b)
+splitPackages = splitUsing $ \x -> case snd x of IPackage x -> Just x; _ -> Nothing
+splitModules = splitUsing $ \x -> case snd x of IModule x -> Just x; _ -> Nothing
+
+splitUsing :: (a -> Maybe String) -> [a] -> [(String, [a])]
+splitUsing f = repeatedly $ \(x:xs) ->
+    let (a,b) = break (isJust . f) xs
+    in ((fromMaybe "" $ f x, x:a), b)
 
 
-readOrdering :: IO (Map.Map String Int)
-readOrdering = do
+baseModuleOrder :: String -> Int
+baseModuleOrder x
+    | "GHC." `isPrefixOf` x = maxBound
+    | otherwise = fromMaybe (maxBound-1) $ elemIndex x
+    ["Prelude","Data.List","Data.Maybe","Data.Function","Control.Monad"]
+
+packageOrder :: IO (String -> Int)
+packageOrder = do
     src <- readFile "input/reverse.htm"
-    return $ Map.fromList $ f $ lines src
+    let mp = Map.fromList $ f $ lines src
+    return $ maybe 0 negate . flip Map.lookup mp
     where
         f (x:"</td>":y:xs)
             | Just x <- stripPrefix "<tr><td><a href=\"http://packdeps.haskellers.com/reverse/" x
