e5faf3d1495b5eebc50aa1fffbff643d4b4f5552
Neil Mitchell <ndmitchell@gmail.com>
2015-01-17 14:23:58 +0000

    Add score weighted results, with a bounded memory heap

--- src/Action/Search.hs
+++ src/Action/Search.hs
@@ -7,6 +7,7 @@ import Data.List.Extra
 import System.FilePath
 import Control.Monad.Extra
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 import Output.Items
 import Output.Tags
@@ -15,6 +16,8 @@ import Output.Types
 import Query
 import Input.Type
 import Action.CmdLine
+import General.Util
+
 
 -- -- generate all
 -- @tagsoup -- generate tagsoup
@@ -41,4 +44,16 @@ search pkg (Query qtags strs typ) = do
         (xs, Just t ) -> do
             nam <- Set.fromList <$> searchNames pkg xs
             filter (`Set.member` nam) <$> searchTypes pkg t
-    mapM (lookupItem pkg) $ take 25 $ filter (filterTags tags qtags) $ map snd is
+    mapM (lookupItem pkg . snd) $ takeScore 25 $ filter (filterTags tags qtags . snd) is
+
+takeScore :: Int -> [(Score, a)] -> [(Score,a)]
+takeScore = f 0 Map.empty
+    where
+        -- Map is Map Score [a], and nmp is the count of items in all list
+        f nmp mp i xs | i <= 0 = []
+        f nmp mp i [] = take i [(s,y) | (s,ys) <- Map.toAscList mp, y <- ys]
+        f nmp mp i xs | nmp > i, Just ((s,ys),mp) <- Map.maxViewWithKey mp =
+            let (die,keep) = splitAt (nmp - i) ys
+            in f (nmp-length die) (if null keep then mp else Map.insert s keep mp) i xs
+        f nmp mp i ((0,x):xs) = (0,x) : f nmp mp (i-1) xs
+        f nmp mp i ((s,x):xs) = f (nmp+1) (Map.insertWith (++) s [x] mp) i xs
