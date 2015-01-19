3a453fdda97ee5626ccb8ad54756d89c317bc871
Neil Mitchell <ndmitchell@gmail.com>
2015-01-19 10:05:55 +0000

    Improve the Item read function a lot, make it as powerful as the one in Hoogle

--- src/Input/Type.hs
+++ src/Input/Type.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE ViewPatterns #-}
 
 module Input.Type(
     Database(..),
-    ItemEx(..), Item(..), showItems, readItems,
+    ItemEx(..), Item(..),
+    showItem, prettyItem, readItem,
     isIPackage, isIModule,
     URL, Documentation,
     Id(..)
@@ -10,6 +12,7 @@ module Input.Type(
 import Numeric
 import Data.Tuple.Extra
 import Language.Haskell.Exts
+import Data.List
 import General.Util
 
 newtype Database = Database FilePath
@@ -45,12 +48,37 @@ isIModule IModule{} = True; isIModule _ = False
 isIPackage IPackage{} = True; isIPackage _ = False
 
 
-showItems :: Item -> String
-showItems (IKeyword x) = "keyword " ++ x
-showItems (IPackage x) = "package " ++ x
-showItems (IModule x) = "module "
-showItems (IDecl x) = pretty x
+---------------------------------------------------------------------
+-- ITEM AS STRING
 
+showItem :: Item -> String
+showItem (IKeyword x) = "@keyword " ++ x
+showItem (IPackage x) = "@package " ++ x
+showItem (IModule x) = "module "
+showItem (IDecl x) = pretty x
 
-readItems :: String -> Maybe Item
-readItems = undefined
+prettyItem :: Item -> String
+prettyItem (IKeyword x) = "keyword " ++ x
+prettyItem (IPackage x) = "package " ++ x
+prettyItem x = showItem x
+
+readItem :: String -> Maybe Item
+readItem (stripPrefix "@keyword " -> Just x) = Just $ IKeyword x
+readItem (stripPrefix "@package " -> Just x) = Just $ IPackage x
+readItem (stripPrefix "module " -> Just x) = Just $ IModule x
+readItem x | ParseOk y <- parseDeclWithMode parseMode x = Just $ IDecl y
+readItem x -- newtype
+    | Just x <- stripPrefix "newtype " x
+    , ParseOk (DataDecl a _ c d e f g) <- parseDeclWithMode parseMode $ "data " ++ x
+    = Just $ IDecl $ DataDecl a NewType c d e f g
+readItem x -- constructors
+    | ParseOk (GDataDecl _ _ _ _ _ _ [GadtDecl s name _ ty] _) <- parseDeclWithMode parseMode $ "data Data where " ++ x
+    = Just $ IDecl $ TypeSig s [name] ty
+readItem o@('(':xs) -- tuple definitions
+    | ")" `isPrefixOf` rest
+    , ParseOk y <- parseDeclWithMode parseMode $ replicate (length com + 2) 'a' ++ drop 1 rest
+    = Just $ IDecl $ f y
+    where
+        (com,rest) = span (== ',') xs
+        f (TypeSig s [Ident _] ty) = TypeSig s [Ident $ '(':com++")"] ty
+readItem _ = Nothing
