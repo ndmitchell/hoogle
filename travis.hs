2032d8c5d1290679852f2ac48fa64ce2e60a8fad
Neil Mitchell <ndmitchell@gmail.com>
2015-01-20 08:08:31 +0000

    Move generate before test when checking the program

--- travis.hs
+++ travis.hs
@@ -10,8 +10,8 @@ main = do
         (time,_) <- duration $ system_ $ "hogle +RTS -T -RTS " ++ (if x == "" then "" else show x)
         putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
         putStrLn "\n\n"
-    timed "test"
     timed "generate"
+    timed "test"
     timed "map"
     timed "map package:base"
     timed ":: a -> b"
