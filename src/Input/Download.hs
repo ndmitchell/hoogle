40f8808be58d4a0abe69af3a1498e6404d80dc09
Neil Mitchell <ndmitchell@gmail.com>
2015-01-26 10:41:30 +0000

    Down't download keywords, is't not used and seems to have gone walkies

--- src/Input/Download.hs
+++ src/Input/Download.hs
@@ -10,8 +10,7 @@ import System.IO
 
 
 urls =
-    [("keywords.htm","https://www.haskell.org/haskellwiki/Keywords")
-    ,("stackage.txt","http://www.stackage.org/lts/cabal.config")
+    [("stackage.txt","http://www.stackage.org/lts/cabal.config")
     ,("platform.txt","https://raw.githubusercontent.com/haskell/haskell-platform/master/hptool/src/Releases2014.hs")
     ,("cabal.tar.gz","http://hackage.haskell.org/packages/index.tar.gz")
     ,("hoogle.tar.gz","http://hackage.haskell.org/packages/hoogle.tar.gz")
