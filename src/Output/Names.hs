765f3374e4a2e0cd282248dd274d2f56e1ee76c3
Neil Mitchell <ndmitchell@gmail.com>
2015-01-30 18:03:55 +0000

    Rewrite names to store a vector and bytestring separately

--- src/Output/Names.hs
+++ src/Output/Names.hs
@@ -4,8 +4,8 @@ module Output.Names(writeNames, searchNames) where
 
 import Data.List.Extra
 import Data.Maybe
-import Data.Tuple.Extra
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Vector.Storable as Vector
 import Data.Typeable
 
 import Input.Type
@@ -17,8 +17,11 @@ data Names = Names deriving Typeable
 
 
 writeNames :: StoreOut -> [(Maybe Id, Item)] -> IO ()
-writeNames store xs = writeStoreType store Names $ writeStoreBS store $ BS.pack $ unlines
-    [show i ++ " " ++ [' ' | isUName name] ++ lower name | (Just i, x) <- xs, name <- toName x]
+writeNames store xs = do
+    let (ids, strs) = unzip [(i, [' ' | isUName name] ++ lower name) | (Just i, x) <- xs, name <- toName x]
+    writeStoreType store Names $ do
+        writeStoreV store $ Vector.fromList ids
+        writeStoreBS store $ BS.unlines $ map BS.pack strs
 
 toName :: Item -> [String]
 toName (IKeyword x) = [x]
@@ -28,22 +31,19 @@ toName (IDecl x) = declNames x
 
 searchNames :: StoreIn -> Bool -> [String] -> IO [(Score, Id)]
 searchNames store exact xs = do
-    let src = BS.lines $ readStoreBS $ readStoreType Names store
-    return $ mapMaybe (match exact xs) src
+    let [v,bs] = readStoreList $ readStoreType Names store
+    return $ mapMaybe (match exact xs) $ zip (Vector.toList $ readStoreV v) (BS.lines $ readStoreBS bs)
 
-match :: Bool -> [String] -> BS.ByteString -> Maybe (Score, Id)
-match exact xs = \line ->
-    let (ident, str) = second (BS.drop 1) $ BS.break (== ' ') line
-        ident2 = read $ BS.unpack ident
-    in fmap (,ident2) $ case () of
-        _ | BS.length str < mn -> Nothing
-          | not $ all (`BS.isInfixOf` str) xsMatch -> Nothing
-          | any (== str) xsPerfect -> Just 0
-          | exact -> Nothing
-          | any (== str) xsGood -> Just 1
-          | any (`BS.isPrefixOf` str) xsPerfect -> Just 2
-          | any (`BS.isPrefixOf` str) xsGood -> Just 3
-          | otherwise -> Just 4
+match :: Bool -> [String] -> (Id, BS.ByteString) -> Maybe (Score, Id)
+match exact xs = \(ident, str) -> fmap (,ident) $ case () of
+    _ | BS.length str < mn -> Nothing
+      | not $ all (`BS.isInfixOf` str) xsMatch -> Nothing
+      | any (== str) xsPerfect -> Just 0
+      | exact -> Nothing
+      | any (== str) xsGood -> Just 1
+      | any (`BS.isPrefixOf` str) xsPerfect -> Just 2
+      | any (`BS.isPrefixOf` str) xsGood -> Just 3
+      | otherwise -> Just 4
     where
         mn = sum $ map BS.length xsMatch
         xsMatch = map (BS.pack . lower) xs
