bf1c1431517a9d62ec639d03042cc59a7d5c4d84
Neil Mitchell <ndmitchell@gmail.com>
2015-02-12 17:56:01 +0000

    Add the non-compiling draft of the API

---
+++ src/Hoogle.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE PatternGuards, ViewPatterns, RecordWildCards #-}
+
+-- Draft of Hoogle API, does not yet compile!
+module Hoogle(
+    Query(..), parseQuery,
+    ItemEx(..), Item(..), showItem,
+    withSearch
+    ) where
+
+import qualified Query as Q
+import qualified Input.Type as I
+import qualified General.Store as S
+import qualified Action.Search as S
+
+
+newtype Database = Database StoreRead
+
+withDatabase :: FilePath -> (Database -> IO ()) -> IO ()
+withDatabase file act = withStoreFile file $ act . Database
+
+search :: Database -> [Query] -> [ItemEx]
+search = undefined
