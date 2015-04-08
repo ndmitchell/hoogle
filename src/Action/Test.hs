25e872600552d1d5dc554f170c4e7935e8935281
Neil Mitchell <ndmitchell@gmail.com>
2015-04-08 07:50:16 +0100

    Add some very coarse --deep testing

--- src/Action/Test.hs
+++ src/Action/Test.hs
@@ -10,12 +10,25 @@ import General.Util
 import Input.Type
 import System.IO.Extra
 
+import Control.Monad
+import Output.Items
+import General.Store
+import Control.DeepSeq
+import Control.Exception
+
 
 actionTest :: CmdLine -> IO ()
-actionTest Test{} = withBuffering stdout NoBuffering $ do
+actionTest Test{..} = withBuffering stdout NoBuffering $ do
+    putStrLn "Quick tests"
     general_util_test
     input_type_test
     query_test
     action_search_test
     action_server_test
     putStrLn ""
+    when deep $ storeReadFile "output/all.hoo" $ \store -> do
+        putStrLn "Deep tests"
+        let xs = map itemItem $ listItems store
+        evaluate $ rnf xs
+        putStrLn $ "Loaded " ++ show (length xs) ++ " items"
+
