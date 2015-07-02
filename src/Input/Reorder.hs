18b69403937e5efd729f89cca4e94410e3955a12
Neil Mitchell <ndmitchell@gmail.com>
2015-07-02 18:02:23 +0100

    Reorder all the items within a module so they are sorted

--- src/Input/Reorder.hs
+++ src/Input/Reorder.hs
@@ -6,16 +6,23 @@ import Input.Type
 import Data.List.Extra
 import Data.Maybe
 import Data.Tuple.Extra
+import General.Util
 
 
 -- | Reorder items so the most popular ones are first, using reverse dependencies
 reorderItems :: (String -> Int) -> [(a, Item)] -> IO [(a, Item)]
 reorderItems packageOrder xs = do
+    let refunc = map $ second $ \(x:xs) -> x : sortOn (itemName . snd) xs
     let rebase (x, xs) | x `elem` ["base","haskell98","haskell2010"]
-                       = (x, concatMap snd $ sortOn ((baseModuleOrder &&& id) . fst) $ splitIModule xs)
-        rebase (x, xs) = (x, concatMap snd $ sortOn fst $ splitIModule xs)
+                       = (x, concatMap snd $ sortOn ((baseModuleOrder &&& id) . fst) $ refunc $ splitIModule xs)
+        rebase (x, xs) = (x, concatMap snd $ sortOn fst $ refunc $ splitIModule xs)
     return $ concatMap snd $ sortOn ((packageOrder &&& id) . fst) $ map rebase $ splitIPackage xs
 
+itemName :: Item -> String
+itemName (IDecl x) = head $ declNames x ++ [""]
+itemName (IKeyword x) = x
+itemName (IPackage x) = x
+itemName (IModule x) = x
 
 baseModuleOrder :: String -> Int
 baseModuleOrder x
