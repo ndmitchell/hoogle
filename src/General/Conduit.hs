e4172da1ab64f43185e93783ce1928d3aee161b5
Neil Mitchell <ndmitchell@gmail.com>
2015-07-21 16:19:13 +0100

    Remove the BangPatterns extension

--- src/General/Conduit.hs
+++ src/General/Conduit.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoMonomorphismRestriction, PatternGuards, Rank2Types, CPP, BangPatterns #-}
+{-# LANGUAGE NoMonomorphismRestriction, PatternGuards, Rank2Types, CPP #-}
 {-# OPTIONS_GHC -fno-warn-warnings-deprecations #-} -- QSem was deprecated in 7.6, but then undeprecated
 
 module General.Conduit(
