2e6f14b7d5ec5d29795d8e3e24a4ac6a18312f07
Neil Mitchell <ndmitchell@gmail.com>
2015-07-24 18:05:39 +0100

    Add an NFData instance to IString, ensure all strings are fully evaluated before creating the IString

--- src/General/IString.hs
+++ src/General/IString.hs
@@ -7,6 +7,7 @@ module General.IString(
 
 import Data.Data
 import Data.IORef
+import Control.DeepSeq
 import Data.String
 import qualified Data.Map as Map
 import System.IO.Unsafe
@@ -26,6 +27,7 @@ instance Ord IString where
 instance Show IString where show = fromIString
 instance Read IString where readsPrec _ x = [(toIString x,"")]
 instance IsString IString where fromString = toIString
+instance NFData IString where rnf (IString _ _) = () -- we force the string at construction time
 
 {-# NOINLINE istrings #-}
 istrings :: IORef (Map.Map String IString)
@@ -35,6 +37,6 @@ fromIString :: IString -> String
 fromIString (IString _ x) = x
 
 toIString :: String -> IString
-toIString x = unsafePerformIO $ atomicModifyIORef istrings $ \mp -> case Map.lookup x mp of
+toIString x | () <- rnf x = unsafePerformIO $ atomicModifyIORef istrings $ \mp -> case Map.lookup x mp of
     Just v -> (mp, v)
     Nothing -> let res = IString (Map.size mp) x in (Map.insert x res mp, res)
