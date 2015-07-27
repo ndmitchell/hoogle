e2af75d97a0ac8f7bbcb0b625941d61fa5aad0f4
Neil Mitchell <ndmitchell@gmail.com>
2015-07-27 07:32:26 +0100

    Expose toSig from Input.Type

--- src/Input/Type.hs
+++ src/Input/Type.hs
@@ -2,11 +2,16 @@
 
 -- | Types used to generate the input.
 module Input.Type(
-    Sig(..), Ctx(..), Ty(..)
+    Sig(..), Ctx(..), Ty(..),
+    toSig
     ) where
 
 import Data.Data
 import Control.DeepSeq
+import Language.Haskell.Exts
+import General.Util
+import General.IString
+import Data.Maybe
 
 
 -- FIXME: Delete the Read instances
@@ -19,3 +24,39 @@ instance NFData n => NFData (Ctx n) where rnf (Ctx x y) = rnf x `seq` rnf y
 instance NFData n => NFData (Ty  n) where
     rnf (TCon x y) = rnf x `seq` rnf y
     rnf (TVar x y) = rnf x `seq` rnf y
+
+
+toSig :: Type -> Sig IString
+toSig = tyForall
+    where
+        -- forall at the top is different
+        tyForall (TyParen x) = tyForall x
+        tyForall (TyForall _ c t) | Sig cs ts <- tyForall t = Sig (toCtx c ++ cs) ts
+        tyForall x = Sig [] $ tyFun x
+
+        tyFun (TyParen x) = tyFun x
+        tyFun (TyFun a b) = ty a : tyFun b
+        tyFun x = [ty x]
+
+        ty (TyForall _ _ x) = TCon "\\/" [ty x]
+        ty x@TyFun{} = TCon "->" $ tyFun x
+        ty (TyTuple box ts) = TCon (toIString $ fromQName $ Special $ TupleCon box $ length ts) (map ty ts)
+        ty (TyList x) = TCon "[]" [ty x]
+        ty (TyParArray x) = TCon "[::]" [ty x]
+        ty (TyApp x y) = case ty x of
+            TCon a b -> TCon a (b ++ [ty y])
+            TVar a b -> TVar a (b ++ [ty y])
+        ty (TyVar x) = TVar (toIString $ fromName x) []
+        ty (TyCon x) = TCon (toIString $ fromQName x) []
+        ty (TyInfix a b c) = ty $ TyCon b `TyApp` a `TyApp` c
+        ty (TyKind x _) = ty x
+        ty (TyBang _ x) = ty x
+        ty _ = TVar "_" []
+
+toCtx :: Context -> [Ctx IString]
+toCtx = mapMaybe f
+    where
+        f (ParenA x) = f x
+        f (InfixA a con b) = f $ ClassA con [a,b]
+        f (ClassA con (TyVar var:_)) = Just $ Ctx (toIString $ fromQName con) (toIString $ fromName var)
+        f _ = Nothing
