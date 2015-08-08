a111e06a953f5444790910add817e203010b2517
Neil Mitchell <ndmitchell@gmail.com>
2015-08-08 21:25:26 +0100

    Add -N2 when upgrading

--- script/Upgrade.hs
+++ script/Upgrade.hs
@@ -40,7 +40,7 @@ main = do
         echo system_ "cabal configure \"--ghc-options=-rtsopts -O2 -fno-state-hack -threaded\""
         echo system_ "cabal build"
         let exe = normalise "dist/build/hoogle/hoogle"
-        echo system_ $ "hoogle_datadir=. " ++ exe ++ " generate --database=default.hoo +RTS -M1.5G -T"
+        echo system_ $ "hoogle_datadir=. " ++ exe ++ " generate --database=default.hoo +RTS -M1.5G -T -N2"
         echo system_ $ exe ++ " test --database=default.hoo"
         ignore $ echo system_ "pkill hoogle"
         let cmd = "nohup " ++ exe ++ " server --database=default.hoo --port=8080 " ++
