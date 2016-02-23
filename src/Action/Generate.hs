56511579f15bed3a8b7554e920e2fffd4fbd0706
Neil Mitchell <ndmitchell@gmail.com>
2016-02-23 17:15:04 +0000

    Push the timing pieces into a separate utility module

--- src/Action/Generate.hs
+++ src/Action/Generate.hs
@@ -5,7 +5,6 @@ module Action.Generate(actionGenerate) where
 import Data.List.Extra
 import System.FilePath
 import System.Directory.Extra
-import System.Time.Extra
 import Data.Tuple.Extra
 import Control.Exception.Extra
 import Data.IORef
@@ -14,7 +13,6 @@ import qualified Data.Set as Set
 import qualified Data.Map as Map
 import qualified Data.Text as T
 import Control.Monad.Extra
-import Numeric.Extra
 import System.Console.CmdArgs.Verbosity
 import Prelude
 
@@ -30,6 +28,7 @@ import Input.Set
 import Input.Item
 import General.Util
 import General.Store
+import General.Timing
 import General.Str
 import System.Mem
 import System.IO
@@ -89,40 +88,6 @@ generate output metadata  = undefined
 -- @tagsoup -- generate tagsoup
 -- @tagsoup filter -- search the tagsoup package
 -- filter -- search all
-
-data Timing = Timing (Maybe (IORef [(String, Double)]))
-
-withTiming :: Maybe FilePath -> (Timing -> IO a) -> IO a
-withTiming file f = do
-    offset <- offsetTime
-    ref <- newIORef []
-    res <- f $ Timing $ if isJust file then Just ref else Nothing
-    end <- offset
-    whenJust file $ \file -> do
-        ref <- readIORef ref
-        -- Expecting unrecorded of ~2s
-        -- Most of that comes from the pipeline - we get occasional 0.01 between items as one flushes
-        -- Then at the end there is ~0.5 while the final item flushes
-        ref <- return $ reverse $ sortOn snd $ ("Unrecorded",end - sum (map snd ref)) : ref
-        writeFile file $ unlines [showDP 2 b ++ "\t" ++ a | (a,b) <- ("Total",end) : ref]
-    putStrLn $ "Took " ++ showDuration end
-    return res
-
-
-timed :: MonadIO m => Timing -> String -> m a -> m a
-timed (Timing ref) msg act = do
-    liftIO $ putStr (msg ++ "... ") >> hFlush stdout
-    time <- liftIO offsetTime
-    res <- act
-    time <- liftIO time
-    stats <- liftIO getGCStatsEnabled
-    s <- if not stats then return "" else do GCStats{..} <- liftIO getGCStats; return $ " (" ++ show peakMegabytesAllocated ++ "Mb)"
-    liftIO $ putStrLn $ showDuration time ++ s
-    case ref of -- don't use whenJust, induces Appliative pre-AMP
-        Nothing -> return ()
-        Just ref -> liftIO $ modifyIORef ref ((msg,time):)
-    return res
-
 
 readRemote :: CmdLine -> Timing -> IO (Map.Map String Package, Set.Set String, Source IO (String, URL, LStr))
 readRemote Generate{..} timing = do
---
+++ src/General/Timing.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module General.Timing(Timing, withTiming, timed) where
+
+import Data.List.Extra
+import System.Time.Extra
+import Data.IORef
+import Data.Maybe
+import Control.Monad.Extra
+import Numeric.Extra
+import System.IO
+import GHC.Stats
+import Control.Monad.IO.Class
+
+
+data Timing = Timing (Maybe (IORef [(String, Double)]))
+
+withTiming :: Maybe FilePath -> (Timing -> IO a) -> IO a
+withTiming file f = do
+    offset <- offsetTime
+    ref <- newIORef []
+    res <- f $ Timing $ if isJust file then Just ref else Nothing
+    end <- offset
+    whenJust file $ \file -> do
+        ref <- readIORef ref
+        -- Expecting unrecorded of ~2s
+        -- Most of that comes from the pipeline - we get occasional 0.01 between items as one flushes
+        -- Then at the end there is ~0.5 while the final item flushes
+        ref <- return $ reverse $ sortOn snd $ ("Unrecorded",end - sum (map snd ref)) : ref
+        writeFile file $ unlines [showDP 2 b ++ "\t" ++ a | (a,b) <- ("Total",end) : ref]
+    putStrLn $ "Took " ++ showDuration end
+    return res
+
+
+timed :: MonadIO m => Timing -> String -> m a -> m a
+timed (Timing ref) msg act = do
+    liftIO $ putStr (msg ++ "... ") >> hFlush stdout
+    time <- liftIO offsetTime
+    res <- act
+    time <- liftIO time
+    stats <- liftIO getGCStatsEnabled
+    s <- if not stats then return "" else do GCStats{..} <- liftIO getGCStats; return $ " (" ++ show peakMegabytesAllocated ++ "Mb)"
+    liftIO $ putStrLn $ showDuration time ++ s
+    case ref of -- don't use whenJust, induces Appliative pre-AMP
+        Nothing -> return ()
+        Just ref -> liftIO $ modifyIORef ref ((msg,time):)
+    return res
