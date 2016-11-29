f191ad6b4d828f556834ca190131a80c80830a0c
Neil Mitchell <ndmitchell@gmail.com>
2016-11-29 20:45:58 +0000

    #196, remove an unnecessary import

--- src/Action/Generate.hs
+++ src/Action/Generate.hs
@@ -15,7 +15,6 @@ import qualified Data.Map as Map
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Control.Monad.Extra
-import Control.Applicative
 import Data.Monoid
 import System.Console.CmdArgs.Verbosity
 import Prelude
