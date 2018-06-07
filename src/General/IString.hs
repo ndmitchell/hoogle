af505e362a670e3ad793ab54748c17a97548260c
Neil Mitchell <ndmitchell@gmail.com>
2018-06-07 23:39:05 +0100

    Switch IString to using foundation strings, saves about 25Mb of memory

--- src/General/IString.hs
+++ src/General/IString.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE PatternGuards, DeriveDataTypeable #-}
+{-# LANGUAGE PatternGuards, DeriveDataTypeable, ViewPatterns, BangPatterns #-}
 
 -- | Interned strings
 module General.IString(
@@ -9,34 +9,35 @@ import Data.Data
 import Data.IORef
 import Control.DeepSeq
 import Data.String
+import qualified Foundation as Fdn
 import qualified Data.Map as Map
 import System.IO.Unsafe
 
 
-data IString = IString {-# UNPACK #-} !Int !String
+data IString = IString {-# UNPACK #-} !Int !Fdn.String String
     deriving (Data,Typeable)
 
 instance Eq IString where
-    IString x _ == IString y _ = x == y
+    IString x _ _ == IString y _ _ = x == y
 
 instance Ord IString where
-    compare (IString x1 x2) (IString y1 y2)
+    compare (IString x1 x2 _) (IString y1 y2 _)
         | x1 == y1 = EQ
         | otherwise = compare x2 y2
 
 instance Show IString where show = fromIString
 instance Read IString where readsPrec _ x = [(toIString x,"")]
 instance IsString IString where fromString = toIString
-instance NFData IString where rnf (IString _ _) = () -- we force the string at construction time
+instance NFData IString where rnf (IString _ _ _) = () -- we force the string at construction time
 
 {-# NOINLINE istrings #-}
-istrings :: IORef (Map.Map String IString)
+istrings :: IORef (Map.Map Fdn.String IString)
 istrings = unsafePerformIO $ newIORef Map.empty
 
 fromIString :: IString -> String
-fromIString (IString _ x) = x
+fromIString (IString _ _ x) = x
 
 toIString :: String -> IString
-toIString x | () <- rnf x = unsafePerformIO $ atomicModifyIORef istrings $ \mp -> case Map.lookup x mp of
+toIString (fromString -> !x) = unsafePerformIO $ atomicModifyIORef istrings $ \mp -> case Map.lookup x mp of
     Just v -> (mp, v)
-    Nothing -> let res = IString (Map.size mp) x in (Map.insert x res mp, res)
+    Nothing -> let res = IString (Map.size mp) x (Fdn.toList x) in (Map.insert x res mp, res)
