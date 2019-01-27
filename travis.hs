7894a80348e66fe6cf370656b0e57532f393a5ba
Neil Mitchell <ndmitchell@gmail.com>
2019-01-27 16:38:23 +0000

    Bump the stack limit

--- travis.hs
+++ travis.hs
@@ -8,7 +8,7 @@ main :: IO ()
 main = do
     let timed x = do
         putStrLn $ "\n\nSearching for " ++ x
-        (time,_) <- duration $ system_ $ "hoogle +RTS -T -M1.8G -K1000K -RTS " ++ show x
+        (time,_) <- duration $ system_ $ "hoogle +RTS -T -M1.8G -K2000K -RTS " ++ show x
         putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
         putStrLn "\n\n"
     retry 3 $ timed "generate"
