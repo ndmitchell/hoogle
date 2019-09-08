73fa6b5c156e0015e135a564e2821719611abe03
Neil Mitchell <ndmitchell@gmail.com>
2019-09-08 23:38:53 +0100

    Make sure that ghc takes precedence over ghc-lib-*

--- src/Input/Reorder.hs
+++ src/Input/Reorder.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
 
 module Input.Reorder(reorderItems) where
 
@@ -10,10 +10,21 @@ import General.Util
 import General.Str
 
 
--- | Reorder items so the most popular ones are first, using reverse dependencies
+pkgGhc :: PkgName
+pkgGhc = strPack "ghc"
+
+packageOrderHacks :: (PkgName -> Int) -> PkgName -> Int
+-- 'ghc' is the canonical module that both 'ghc-lib-parser' and 'ghc-lib' copy from, so better to pick that
+-- even though ghc-lib-* are used more on Stackage (but a lot less on Hackage)
+packageOrderHacks f x | x == pkgGhc = min (f x) $ min (f $ strPack "ghc-lib-parser") (f $ strPack "ghc-lib") - 1
+packageOrderHacks f x = f x
+
+
+-- | Reorder items so the most popular ones are first, using reverse dependencies.
+--   Low numbers for the PkgName function mean the package is more popular.
 reorderItems :: Settings -> (PkgName -> Int) -> [(a, Item)] -> [(a, Item)]
 reorderItems Settings{..} packageOrder xs =
-    concatMap snd $ sortOn ((packageOrder &&& id) . fst) $ map rebase $ splitIPackage xs
+    concatMap snd $ sortOn ((packageOrderHacks packageOrder &&& id) . fst) $ map rebase $ splitIPackage xs
     where
         refunc = map $ second $ \(x:xs) -> x : sortOn (itemName . snd) xs
         rebase (x, xs) = (x, concatMap snd $ sortOn (((negate . f . strUnpack) &&& id) . fst) $ refunc $ splitIModule xs)
