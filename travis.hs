88aa27213a8c7a653d97b12672831750be22e0ba
Neil Mitchell <ndmitchell@gmail.com>
2020-11-23 12:38:49 +0000

    Indent the Travis script more

--- travis.hs
+++ travis.hs
@@ -7,10 +7,10 @@ import Control.Exception.Extra
 main :: IO ()
 main = do
     let timed x = do
-        putStrLn $ "\n\nSearching for " ++ x
-        (time,_) <- duration $ system_ $ "hoogle +RTS -T -M1.8G -K2000K -RTS " ++ show x
-        putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
-        putStrLn "\n\n"
+            putStrLn $ "\n\nSearching for " ++ x
+            (time,_) <- duration $ system_ $ "hoogle +RTS -T -M1.8G -K2000K -RTS " ++ show x
+            putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
+            putStrLn "\n\n"
     retry 3 $ timed "generate"
     timed "test"
     timed "map"
