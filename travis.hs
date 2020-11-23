fb7bb6d191a383adfe7017588f14efa058e3d9b4
Neil Mitchell <ndmitchell@gmail.com>
2020-11-23 14:07:35 +0000

    Put a bit of sleep in the retry loop

--- travis.hs
+++ travis.hs
@@ -11,10 +11,10 @@ main = do
             (time,_) <- duration $ system_ $ "hoogle +RTS -T -M1.8G -K2000K -RTS " ++ show x
             putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
             putStrLn "\n\n"
-    retry 3 $ timed "generate"
+    retry 3 $ timed "generate" >> sleep 10
     timed "test"
     timed "map"
     timed "map package:base"
     timed ":: a -> b"
     timed ":: ShakeOptions -> a"
-    retry 3 $ timed "generate --frege"
+    retry 3 $ timed "generate --frege" >> sleep 10
