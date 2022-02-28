aea27c4a92fa380e3df2ccde88e0ece2a3c85a44
PaulAbloescher <abloescherpaul@gmail.com>
2022-02-28 17:53:22 +0100

    Ignore hoogleIds which are not in index.

--- src/Rank/ranking/storage/document_store.py
+++ src/Rank/ranking/storage/document_store.py
@@ -7,14 +7,15 @@ class DocumentStore:
         self._df = jl.read_dataset(dataset_file)
 
     def get_storage_ids_for_hoogle_ids(self, hoogle_ids):
+        hoogle_ids = [hoogle_id for hoogle_id in hoogle_ids if hoogle_id in self._df.index]
         return self._df.loc[hoogle_ids].index.get_level_values('storageId').unique().values
 
     def get_storage_id_for_hoogle_id(self, hoogle_id):
         [storageId] = self.get_storage_ids_for_hoogle_ids([hoogle_id])
         return storageId
 
-    def get_doc_by_hoogle_id(self, hoogle_id):
-        return self._df.loc[hoogle_id]
+    def get_doc_contents_by_hoogle_ids(self, hoogle_id):
+        return self._df.xs(hoogle_id, level='storageId')
 
     def read_corpus(self) -> np.ndarray:
         groups = self._df.groupby('storageId')
