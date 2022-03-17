ad4edbc69e717788494b2b779e0ca18cda971d2d
PaulAbloescher <abloescherpaul@gmail.com>
2022-03-17 17:42:38 +0100

    update dataset paths to absolute paths.

--- src/Rank/ranking/util/dataset_paths.py
+++ src/Rank/ranking/util/dataset_paths.py
@@ -1,15 +1,15 @@
 import os
 
-corpora_dir = '..\\..\\datasets\\corpora'
-evaluation_dir = '..\\..\\datasets\\evaluation'
+corpora_dir = os.path.abspath('..\\..\\datasets\\corpora\\without-code-examples')
+evaluation_dir = os.path.abspath('..\\..\\datasets\\evaluation')
 
 # Corpora
-lemmatized_unique_functions_corpus = os.path.join(corpora_dir, 'lem-test-all-unique-functions.jsonl')
-unique_functions_corpus = os.path.join(corpora_dir, 'test-all-unique-functions.jsonl')
-raw_corpus = os.path.join(corpora_dir, 'raw.docs.jsonl')
+lemmatized_unique_functions_corpus = os.path.join(corpora_dir, 'lem-all-unique-functions.jsonl')
+tokenized_unique_functions_corpus = os.path.join(corpora_dir, 'tokenized-all-unique-functions.jsonl')
+unique_functions_corpus = os.path.join(corpora_dir, 'all-unique-functions.jsonl')
+raw_corpus = os.path.join(corpora_dir, 'no-code-ex-raw.dump.jsonl')
 
 # Evaluation
-hoogle_results = os.path.join(evaluation_dir, 'eval-result-test.jsonl')
-unified_hoogle_results = os.path.join(evaluation_dir, 'eval-plain-hoogle-results.jsonl')
-tf_idf_evaluation_set = os.path.join(evaluation_dir, 'test-tfidf-evalset.jsonl')
+lemmatized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'lemmatized-eval-plain-hoogle-results.jsonl')
+tokenized_tfidf_evaluation_set = os.path.join(evaluation_dir, 'tokenized-eval-plain-hoogle-results.jsonl')
 manual_evaluation_set = os.path.join(evaluation_dir, 'ManualEvalSet.txt')
