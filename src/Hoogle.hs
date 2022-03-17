14519b85ca3f07edc443c0c60a355421bd04bf90
PaulAbloescher <abloescherpaul@gmail.com>
2022-03-17 16:23:19 +0100

    Make runEvaluation create new file when its target does not exist.

--- src/Hoogle.hs
+++ src/Hoogle.hs
@@ -21,6 +21,7 @@ import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified EvalItem as E (readEvalItems, EvalItem (docQuery, queryRes, docType), writeEvalItems, toJson)
 import EvalItem (writeEvalItems, EvalItem (queryRes))
 import System.IO (withFile, IOMode (WriteMode, AppendMode))
+import System.Directory (doesFileExist)
 
 -- | Database containing Hoogle search data.
 newtype Database = Database StoreRead
@@ -62,22 +63,25 @@ putSearchDocs q = do
 
 runEvaluation :: FilePath -> IO ()
 runEvaluation f = do
-  let destination = ".\\eval-result-test.jsonl"
-  startAt <- length . lines <$> readFile destination
+  let destination = ".\\missing-eval-result-test.jsonl"
+  destExists <- doesFileExist destination
+  startAt <- if destExists then length . lines <$> readFile destination else return 0
   print startAt
   database <- defaultDatabaseLocation
   withSearch database $ \store -> do
     items <- E.readEvalItems f
     let enumeratedItems = drop startAt $ zip [1..] items
     let getTargetIds q = map (\(TargetId id, _) -> id) $ searchDatabase' store q
-    withFile destination AppendMode $ \handle ->
+    let mode = if destExists then AppendMode else WriteMode
+    withFile destination mode $ \handle ->
       mapM_ (\(n, i) -> do
         print $ "start item n" ++ show n
         let res = getTargetIds $ E.docType i
         LBS.hPutStrLn handle $ E.toJson $ i { queryRes = Just res }
         ) enumeratedItems
 
-evlpath = ".\\src\\Rank\\notebooks\\data_preparation\\test-tfidf-evalset.jsonl"
+evlpath = ".\\src\\Rank\\notebooks\\data_preparation\\missing-tfidf-evalset.jsonl"
+
 
 -- | Run a command line Hoogle operation.
 hoogle :: [String] -> IO ()
