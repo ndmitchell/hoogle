533cd3e7853331796bdee6e39ced2d1b0aa46105
PaulAbloescher <abloescherpaul@gmail.com>
2022-03-25 08:41:31 +0100

    Add plugin demo samples.

--- src/Plugin/PluginDemo.hs
+++ src/Plugin/PluginDemo.hs
@@ -7,5 +7,8 @@ import Plugin.HolePlugin ()
 import Data.Char (toUpper, isUpper)
 import Data.Text
 
-test :: Text -> Text 
-test x = _turn_around x
+test_one :: Text -> Text 
+test_one x = _remove_leading_whitespace x
+
+test_two :: Text -> Text 
+test_two x = _turn_around x
